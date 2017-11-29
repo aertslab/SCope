9146d02f971645992eaba9797d6d2fa0431347bc
Maxime De Waegeneer <maxime.dewaegeneer@kuleuven.vib.be>
2017-11-29 01:12:33 +0100

    Remove sample functions. Add query functions

--- Client/Client.js
+++ Client/Client.js
@@ -3,26 +3,17 @@ var PROTO_PATH = 'Proto/SCope.proto';
 var grpc = require('grpc');
 var scope_proto = grpc.load(PROTO_PATH).scope;
 
-var params = { n1: 20, n2: 10 };
+var params = { filepath: "/home/luna.kuleuven.be/u0113561/Desktop/FlyBrainProject/FB_20170919_LD.loom"
+             , feature: "gene"
+             , entry: "pros"
+             , logtransform: true };
 
 function main() {
   
   var client = new scope_proto.SCopeSearch('localhost:50050', grpc.credentials.createInsecure());
 
-  client.divide(params, function (err, response) {
-    console.log(response.f1);
-  });
-
-  client.multiply(params, function (err, response) {
-    console.log(response.n1);
-  });
-
-  client.substract(params, function (err, response) {
-    console.log(response.n1);
-  });
-
-  client.add(params, function (err, response) {
-    console.log(response.n1);
+  client.query(params, function (err, response) {
+    console.log(response.v);
   });
 
 }
