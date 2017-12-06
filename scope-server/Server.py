479daa2dea8f283a399bc575dd77349b0a4528dd
Maxime De Waegeneer <maxime.dewaegeneer@kuleuven.vib.be>
2017-12-06 01:06:26 +0100

    Add the x-Server to the bundled G-Server

--- scope-server/Server.py
+++ scope-server/Server.py
@@ -1,9 +1,16 @@
 import threading
-import GServer as gs
-import PServer as ps
+
+import sys
+sys.path.append("/home/luna.kuleuven.be/u0113561/Projects/Programming/SCope/scope-server/slaves/gserver")
+
+from slaves.gserver import GServer as gs
+from slaves.pserver import PServer as ps
+from slaves.xserver import XServer as xs
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=gs.serve)
     t2 = threading.Thread(target=ps.run)
+    t3 = threading.Thread(target=xs.run)
     t1.start()
     t2.start()
+    t3.start()
