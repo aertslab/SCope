535aec5f2dad4d20805596f14e99a1e9bd77a1ae
Maxime De Waegeneer <maxime.dewaegeneer@kuleuven.vib.be>
2017-12-06 01:06:59 +0100

    Initial commit

---
+++ scope-client/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const webpack = require('webpack')
+
+let config = {
+    entry: './src/main.jsx',
+    output: {
+        path: path.resolve(__dirname, 'assets'),
+        filename: 'main.js',
+        publicPath: '/assets/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            loader: ['react-hot-loader/webpack', 'babel-loader']
+        },
+        {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        },
+        {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: true
+            }
+        })
+    )
+    config.plugins.push(
+        new webpack.optimize.OccurrenceOrderPlugin()
+    )
+} else {
+    config.plugins.push(new webpack.NamedModulesPlugin())
+    config.entry = ['react-hot-loader/patch', config.entry]
+}
+
+// config.node = { fs: 'empty', child_process: 'empty' };
+
+module.exports = config
