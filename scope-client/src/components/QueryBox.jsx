20b27954b2d85faf2091057a895578ee82bd48ee
Maxime De Waegeneer <maxime.dewaegeneer@kuleuven.vib.be>
2017-12-13 18:58:37 +0100

    Initial commit

---
+++ scope-client/src/components/QueryBox.jsx
@@ -0,0 +1,72 @@
+import React, { Component, PropTypes } from 'react'
+import { Segment, Search, Label } from 'semantic-ui-react'
+
+export default class QueryBox extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.GBC = require("grpc-bus-websocket-client");
+
+        this.state = {
+            isLoading: false,
+            results: [],
+            value: ''
+        };
+    }
+
+    componentWillMount() {
+        this.resetComponent()
+    }
+
+    resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
+
+    handleResultSelect = (e, { result }) => {
+        this.setState({ value: result.title })
+        this.props.selectfeature(result.title)
+    }
+
+    handleSearchChange = (e, { value }) => {
+        this.setState({ isLoading: true, value })
+
+        setTimeout(() => {
+            if (this.state.value.length < 1) return this.resetComponent()
+            let query = { 
+                lfp: "/home/luna.kuleuven.be/u0113561/Desktop/FlyBrainProject/FB_20170919_LD.loom"
+                , q: this.state.value 
+            };
+            this.props.gbwccxn.then((gbc) => {
+                gbc.services.scope.Main.getFeatures(query, (err, response) => {
+                    // Subset the top results
+                    let res_top = response.v.slice(0, 10).map((x) => {
+                        return {"title": x}
+                    });
+                    let res = {"gene":{"name":"gene","results":res_top}}
+                    this.setState({
+                        isLoading: false,
+                        results: res,
+                    })
+                });
+            });
+        }, 200)
+    }
+
+    render() {
+
+        const { isLoading, value, results } = this.state
+
+        return (
+            <div>
+                <Search
+                    category
+                    loading={isLoading}
+                    onResultSelect={this.handleResultSelect}
+                    onSearchChange={this.handleSearchChange}
+                    results={results}
+                    value={value}
+                    {...this.props}
+                />
+            </div>
+        );
+    }
+
+}
