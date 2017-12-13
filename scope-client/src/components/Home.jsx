afc5313a05e7f9e3ff278e40678375eec3c58722
Maxime De Waegeneer <maxime.dewaegeneer@kuleuven.vib.be>
2017-12-13 18:58:25 +0100

    Add feature to upload with progress and list the looms. Add feature to select the loom that will be active.

--- scope-client/src/components/Home.jsx
+++ scope-client/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Switch, Router } from 'react-router-dom';
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Grid, Divider, Modal, Label } from 'semantic-ui-react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Grid, Divider, Modal, Label, Progress } from 'semantic-ui-react'
 import FileReaderInput from 'react-file-reader-input';
 import ToggleDisplay from 'react-toggle-display';
 // SCope imports
@@ -18,38 +18,71 @@ export default class Home extends Component {
         this.state = {
             sideBarVisible: true,
             mainVisible: {"welcome":true, "viewer": false},
-            slctdFeature: ""
+            slctdFeature: "",
+            uploadLoomInput: null,
+            uploadProgress: 0,
+            myLooms: [],
+            activeLoom: null,
+            dataPoints: []
         };
         this.GBC = require("grpc-bus-websocket-client");
         this.gbwcCxn = new this.GBC("ws://localhost:8081/", 'src/proto/s.proto', { scope: { Main: 'localhost:50052' } }).connect()
+        this.setMyLooms()
         this.changeTo = this.changeTo.bind(this);
         this.selectFeature = this.selectFeature.bind(this);
+        this.updateUploadProgress = this.updateUploadProgress.bind(this);
     }
 
     toggleVisibility = () => this.setState({ sideBarVisible: !this.state.sideBarVisible })
 
-    uploadLoomFile = (e, results) => {
+    setMyLooms = () => {
+        let query = {}
+        this.gbwcCxn.then((gbc) => {
+            gbc.services.scope.Main.getMyLooms(query, (err, response) => {
+                this.setState({ myLooms: response.l })
+            });
+        });
+    }
+
+    updateUploadProgress = (p) => this.setState({ uploadProgress: p })
+
+    uploadLoomFile = (e, d) => {
+        let f = this.state.uploadLoomInput
+        if(f == null)
+            throw "Please select a .loom file first"
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:50051/');
+        xhr.upload.addEventListener('progress', (e) => {
+            console.log("Data uploaded: " + e.loaded + "/" + e.total)
+            let p = (e.loaded/e.total*100).toPrecision(1)
+            this.updateUploadProgress(p)
+            if(e.loaded == e.total) {
+                console.log('Loom file,'+ f.name +', successfully uploaded !');
+                this.setState({ inputLoom: null })
+            }
+        });
+
+        // xhr.addEventListener('load', () => {
+        //     alert('Loom file,'+ f.name +', successfully uploaded !');
+        //     this.setState({ inputLoom: null })
+        // });
+
+        let form = new FormData();
+        form.append('file', f);
+        xhr.setRequestHeader("Content-Disposition", "attachment;filename=" + f.name)
+        xhr.send(form);
+    }
+
+    selectLoomFile = (e, results) => {
         results.forEach(result => {
             const [e, file] = result;
-            console.log(file)
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', 'http://localhost:50051/');
-            xhr.upload.addEventListener('progress', function (e) {
-                console.log("Data uploaded: " + e.loaded + "/" + e.total)
-                // progress.value = e.loaded;
-                // progress.max = e.total;
-            });
+            this.setState({ uploadLoomInput: file })
+        })
+    }
 
-            xhr.addEventListener('load', function () {
-                alert('Loom file successfully uploaded !');
-            });
-
-            var form = new FormData();
-            form.append('file', file);
-            console.log(file.name)
-            xhr.setRequestHeader("Content-Disposition", "attachment;filename=" + file.name)
-            xhr.send(form);
-        });
+    setActiveLoom = (l) => {
+        this.setState({ activeLoom: l})
+        console.log(l +" is now active!")
     }
 
     changeTo = (p) => {
@@ -70,6 +103,16 @@ export default class Home extends Component {
         const title = { fontSize: 14, width: 150 }
         const screenHeight = window.screen.availHeight
         const mainHeight = screenHeight - 100
+        const sideBarWidth = 250
+
+        let myLooms = this.state.myLooms.map(
+            (l) => {
+                let icon = ""
+                if(l === this.state.activeLoom)
+                    icon = <Icon name="flag" />
+                return (<Menu.Item key={l} onClick={() => this.setActiveLoom(l)}>{l} {icon}</Menu.Item>)
+            }
+        )
 
         return (
             <div>
@@ -86,8 +129,8 @@ export default class Home extends Component {
                         </Menu.Item>
                     </Menu>
                     <Sidebar.Pushable style={{ minHeight: '96vh' }}>
-                        <Sidebar as={Grid} animation='push' width="thin" visible={this.state.sideBarVisible}>
-                            <Menu vertical>
+                        <Sidebar as={Grid} style={{width: sideBarWidth}} animation='push' visible={this.state.sideBarVisible}>
+                            <Menu vertical style={{width: sideBarWidth}}>
                                 <Menu.Item onClick={() => this.changeTo('welcome')}>
                                     <Icon name='home'/>Home
                                 </Menu.Item>
@@ -99,21 +142,25 @@ export default class Home extends Component {
                                     <Divider hidden />
                                     <Menu.Header>FCA DATASETS</Menu.Header>
                                     <Menu.Menu>
-                                        <Menu.Item name='Fly Brain' />
+                                        {/* <Menu.Item name='Fly Brain' /> */}
                                     </Menu.Menu>
                                     <Divider hidden />
                                     <Menu.Header>MY DATASETS</Menu.Header>
                                     <Menu.Menu>
+                                        { myLooms }
                                         <Modal trigger={<Menu.Item><Icon name='attach' />Import .loom</Menu.Item>}>
                                             <Modal.Header>Import a .loom file</Modal.Header>
                                             <Modal.Content image>
                                                 <Modal.Description>
-                                                    <Label>Upload a File:</Label>
+                                                    <Label>Upload a File:</Label>{ this.state.uploadLoomInput != null ? this.state.uploadLoomInput.name : ""}
                                                     <FileReaderInput as="binary" id="my-file-input"
-                                                        onChange={this.uploadLoomFile}>
+                                                        onChange={this.selectLoomFile}>
                                                         <Button>Select a file!</Button>
                                                     </FileReaderInput>
-                                                    <Button>Upload!</Button>
+                                                    <Button onClick={this.uploadLoomFile}>Upload!</Button>
+                                                    <Progress percent={this.state.uploadProgress} indicating progress>
+                                                        The .loom file was successfully uploaded!
+                                                    </Progress>
                                                 </Modal.Description>
                                             </Modal.Content>
                                         </Modal>
@@ -129,7 +176,7 @@ export default class Home extends Component {
                                 <Welcome/>
                             </ToggleDisplay>
                             <ToggleDisplay show={this.state.mainVisible["viewer"]}>
-                                <Viewer feature={this.state.slctdFeature}/>
+                                <Viewer width={window.screen.availWidth - 400} height={window.screen.availHeight - 200} gbwccxn={this.gbwcCxn} loom={this.state.activeLoom} feature={this.state.slctdFeature}/>
                             </ToggleDisplay>
                         </Sidebar.Pusher>
                     </Sidebar.Pushable>
