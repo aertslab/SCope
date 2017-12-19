b88060729e46893daebc1bea40b12355cf3fa221
M.D.W <maxime.dewaegeneer@kuleuven.vib.be>
2017-12-19 14:15:42 +0100

    Initial commit.

---
+++ scope-server/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scope-server',
+      version='0.0.1',
+      description='Server for the SCope software',
+      url='',
+      author='M.D.W',
+      author_email='mdewaegeneer@gmail.com',
+      license='Apache 2.0',
+      packages=['scopeserver'],
+      zip_safe=False)
