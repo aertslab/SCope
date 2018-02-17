45123f60967ac5ac4caf3398c98985140ccbeb90
Lukasz Kreft <lukasz.kreft@vib.be>
2018-02-17 22:00:34 +0100

    fix for starting server under windows

--- scope-server/scopeserver/modules/xserver/XServer.py
+++ scope-server/scopeserver/modules/xserver/XServer.py
@@ -11,4 +11,4 @@ def run(run_event):
     while run_event.is_set():
         print('Starting XServer on port 8081...')
         cwd = os.path.dirname(__file__)
-        subprocess_cmd('cd {0}; node server.js'.format(cwd))
+        subprocess_cmd('cd {0} && node server.js'.format(cwd))
