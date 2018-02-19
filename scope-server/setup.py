06e9724f356ab301781f573b7ef7b06b1099fc54
M.D.W <mdewaegeneer@gmail.com>
2018-02-19 21:59:14 +0100

    Automated installation of XServer by calling python setup.py develop command (issue #17).

--- scope-server/setup.py
+++ scope-server/setup.py
@@ -1,4 +1,21 @@
+from pynpm import NPMPackage
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+class DevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        print("Installing XServer...")
+        pkg = NPMPackage('scopeserver/modules/xserver/package.json')
+        pkg.install()
+        develop.run(self)
+
+class InstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        install.run(self)
 
 setup(name='scope-server',
       entry_points={'console_scripts': [
@@ -6,15 +23,19 @@ setup(name='scope-server',
                         ]
                     },
       version='0.0.1',
-      description='Server for the SCope software',
+      description='SCope Data Server: loading and serving the data to SCope Client',
       url='',
       author='M.D.W',
       author_email='mdewaegeneer@gmail.com',
       license='Apache 2.0',
       packages=['scopeserver'],
       install_requires=[
-          'grpcio',
-          'loompy',
+          'grpcio>=1.7.0',
+          'loompy>=2.0',
           'pandas'
       ],
+      cmdclass={
+        'develop': DevelopCommand,
+        'install': InstallCommand
+      },
       zip_safe=False)
