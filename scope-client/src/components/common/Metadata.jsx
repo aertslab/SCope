98c59ef5accd90b184b5f439b8f8322d58f26f4c
Lukasz Kreft <lukasz.kreft@vib.be>
2018-03-07 14:43:03 +0100

    added cell metadata to lasso selections, fix #50

---
+++ scope-client/src/components/common/Metadata.jsx
@@ -0,0 +1,209 @@
+import React, { Component } from 'react'
+import { Button, Icon, Table, Modal, Dimmer, Loader, Dropdown } from 'semantic-ui-react'
+import { BackendAPI } from '../common/API' 
+
+export default class Metadata extends Component { 
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectionId: props.selectionId,
+			loading: false,
+			annotation: null,
+			clustering: null,
+			metadata : null
+		}
+	}
+
+	render() {
+		const { selectedGenes, selectedRegulons, selectedClusters } = this.getParsedFeatures();
+		const { metadata, selectionId } = this.state;
+		let selections = BackendAPI.getViewerSelections();
+		let selection = selections[selectionId];
+		let loomMetadata = BackendAPI.getActiveLoomMetadata();
+		let annotationOptions = [];
+		let clusteringOptions = [];
+		let selectedClustering = {}, selectedClusteringName = '';
+
+		loomMetadata.cellMetaData.clusterings.map(c => {
+			if (c.id == this.state.clustering) {
+				selectedClusteringName = c.name;
+				c.clusters.map(s => {
+					selectedClustering[s.id] = s.description;
+				})
+			}
+			clusteringOptions.push({text: c.name, value: c.id});
+		})
+
+		loomMetadata.cellMetaData.annotations.map(a => {
+			annotationOptions.push({text: a.name, value: a.name});
+		})
+
+
+		let cellMetadata = selection.points.map((p, j) => {
+			let cells;
+			if (metadata) {
+				return (
+					<Table.Row key={j} textAlign='center' >
+						<Table.Cell>{p}</Table.Cell>
+						<Table.Cell>{metadata.geneExpression[0] ? metadata.geneExpression[0].features[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.geneExpression[1] ? metadata.geneExpression[1].features[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.geneExpression[2] ? metadata.geneExpression[2].features[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.aucValues[0] ? metadata.aucValues[0].features[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.aucValues[1] ? metadata.aucValues[1].features[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.aucValues[2] ? metadata.aucValues[2].features[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.annotations[0] ? metadata.annotations[0].annotations[j] : ''}</Table.Cell>
+						<Table.Cell>{metadata.clusterIDs[0] ? selectedClustering[metadata.clusterIDs[0].clusters[j]] : ''}</Table.Cell>
+					</Table.Row>
+				);
+			}
+			return (
+				<Table.Row key={j} textAlign='center' >
+					<Table.Cell>{p}</Table.Cell>
+					<Table.Cell colSpan={8} /> 
+				</Table.Row>
+			);
+		});
+
+		let tableMetadata = (
+			<Table>
+				<Table.Header>
+					<Table.Row textAlign='center'>
+						<Table.HeaderCell></Table.HeaderCell>
+						<Table.HeaderCell colSpan='3'>Gene expression</Table.HeaderCell>
+						<Table.HeaderCell colSpan='3'>AUC values</Table.HeaderCell>
+						<Table.HeaderCell>Annotation</Table.HeaderCell>
+						<Table.HeaderCell>Clustering</Table.HeaderCell>
+					</Table.Row>
+					<Table.Row textAlign='center'>
+						<Table.HeaderCell>Cell ID</Table.HeaderCell>
+						<Table.HeaderCell>{selectedGenes[0] ? selectedGenes[0] : 'none selected'}</Table.HeaderCell>
+						<Table.HeaderCell>{selectedGenes[1]}</Table.HeaderCell>
+						<Table.HeaderCell>{selectedGenes[2]}</Table.HeaderCell>
+						<Table.HeaderCell>{selectedRegulons[0] ? selectedRegulons[0] : 'none selected'}</Table.HeaderCell>
+						<Table.HeaderCell>{selectedRegulons[1]}</Table.HeaderCell>
+						<Table.HeaderCell>{selectedRegulons[2]}</Table.HeaderCell>
+						<Table.HeaderCell>
+							<Dropdown inline placeholder='select annotation' defaultValue={this.state.annotation} options={annotationOptions} onChange={(p, s) =>{
+								this.setState({annotation: s.value, metadata: null, loading: true});
+								setTimeout(() => {
+									this.getMetadata();
+								}, 50);
+							}} />
+						</Table.HeaderCell>
+						<Table.HeaderCell>
+							<Dropdown inline placeholder='select clustering' defaultValue={this.state.clustering} options={clusteringOptions} onChange={(p, s) => {
+								setTimeout(() => {
+									this.setState({clustering: s.value, metadata: null, loading: true});
+									this.getMetadata();
+								}, 50);
+							}} />
+						</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					{cellMetadata}
+				</Table.Body>
+			</Table>
+		);
+
+		return (
+			<Modal 
+				trigger={<Icon name='search' title='see the metadata for this selection' style={{display: 'inline'}} className="pointer" />}
+				onOpen={() => {
+					setTimeout(() => {
+						this.setState({metadata: null, loading: true})
+						this.getMetadata();
+					}, 50);
+				}}
+				size='large'
+				>
+				<Modal.Content image scrolling>
+					{tableMetadata}
+					<Dimmer active={this.state.loading} inverted>
+	 		 			<Loader inverted>Loading</Loader>
+	 				</Dimmer>			
+				</Modal.Content>
+				<Modal.Actions>
+					<Button primary onClick={() => {
+							let data = [];
+							selection.points.map((p,i) => {
+								let cellData = {};
+								cellData['cellID'] = p;
+								if (metadata) {
+									selectedGenes.map((g, j) => {
+										cellData[g]	= metadata.geneExpression[j].features[i];
+									})
+									selectedRegulons.map((g, j) => {
+										cellData[g]	= metadata.aucValues[j].features[i];
+									})
+									if (metadata.annotations[0]) cellData[this.state.annotation] = metadata.annotations[0].annotations[i];
+									if (metadata.clusterIDs[0]) cellData[selectedClusteringName] = selectedClustering[metadata.clusterIDs[0].clusters[i]];
+								}
+								data.push(cellData);
+							})
+							var fileDownload = require('react-file-download');
+							const json2csv  = require('json2csv').parse;
+							const csv = json2csv(data);
+							fileDownload(csv, 'metadata.csv');
+						}}
+						>
+						<Icon name='download' /> Download 
+					</Button>
+					<Button>
+						<Icon name='close' /> Close 
+					</Button>
+				</Modal.Actions>
+			</Modal>
+		);
+	}
+
+	getParsedFeatures() {
+		let features = BackendAPI.getActiveFeatures();
+		let metadata = BackendAPI.getActiveLoomMetadata();
+		let selectedGenes = [];
+		let selectedRegulons = [];
+		let selectedClusters = [];
+		features.map(f => {
+			if (f.featureType == 'gene') selectedGenes.push(f.feature);
+			if (f.featureType == 'regulon') selectedRegulons.push(f.feature);
+			if (f.featureType.indexOf('Clustering:') == 0) {
+				metadata.cellMetaData.clusterings.map( clustering => {
+					if (f.featureType.indexOf(clustering.name) != -1) {
+						clustering.clusters.map(c => {
+							if (c.description == f.feature) {
+								selectedClusters.push({clusteringName: clustering.name, clusteringID: clustering.id, clusteName: c.name, clusterID: c.id});
+							}
+						})
+					}
+				})
+			}
+		})
+		return { selectedGenes, selectedRegulons, selectedClusters }
+	}
+
+	getMetadata() {
+		let selections = BackendAPI.getViewerSelections();
+		let settings = BackendAPI.getSettings();
+		let loomFilePath = BackendAPI.getActiveLoom();
+		let coordinates = BackendAPI.getActiveCoordinates();
+		const { selectedGenes, selectedRegulons, selectedClusters } = this.getParsedFeatures();
+		let query = {
+			loomFilePath: loomFilePath,
+			cellIndices: selections[this.state.selectionId].points,
+			hasLogTranform: settings.hasLogTransform,
+			hasCpmTranform: settings.hasCpmNormalization,
+			selectedGenes: selectedGenes,
+			selectedRegulons: selectedRegulons,
+			clusterings: this.state.clustering ? [this.state.clustering] : [],
+			annotations: this.state.annotation ? [this.state.annotation] : []
+		}
+		if (DEBUG) console.log('getCellMetaData', query);
+		BackendAPI.getConnection().then((gbc) => {
+			gbc.services.scope.Main.getCellMetaData(query, (err, response) => {
+				if (DEBUG) console.log('getCellMetaData', response);
+				this.setState({loading: false, metadata: response, selection: selections[this.state.selectionId]});
+			});
+		});
+	}
+}
--- scope-client/src/components/common/ViewerSidebar.jsx
+++ scope-client/src/components/common/ViewerSidebar.jsx
@@ -1,136 +1,77 @@
-import _ from 'lodash'
 import React, { Component } from 'react'
 import { Grid, Input, Icon } from 'semantic-ui-react'
 import { BackendAPI } from '../common/API' 
+import Metadata from '../common/Metadata' 
 
 export default class ViewerSidebar extends Component {
-    
-    constructor() {
-        super();
-        this.state = {
-            activePage: BackendAPI.getActivePage(),
-            lassoSelections: BackendAPI.getViewerSelections()
-        }
-        this.featuresListener = (features) => {
-            this.getMetadata(this.state.lassoSelections);
-        }
-        this.selectionsListener = (selections) => {
-            this.setState({lassoSelections: selections});
-            this.getMetadata(selections);
-        }        
-    }
+	
+	constructor() {
+		super();
+		this.state = {
+			activePage: BackendAPI.getActivePage(),
+			lassoSelections: BackendAPI.getViewerSelections()
+		}
+		this.selectionsListener = (selections) => {
+			this.setState({lassoSelections: selections});
+		}        
+	}
 
-    render() {
+	render() {
 
-        let lassoSelections = () => {
-            if(this.state.lassoSelections.length == 0) {
-                return (
-                    <Grid>
-                        <Grid.Column>No user's lasso selections</Grid.Column>
-                    </Grid>
-                );
-            }
-            return (this.state.lassoSelections.map((lS) => {
-                return (
-                    <Grid key={lS.id} columns={3}>
-                        <Grid.Column>
-                            {"Selection "+ lS.id}
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Input
-                                size='mini'
-                                style={{width: 75, height: 10}}
-                                label={{ style: {backgroundColor: '#'+lS.color } }}
-                                labelPosition='right'
-                                placeholder={'#'+lS.color}
-                            />
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Icon name='eye' style={{display: 'inline'}} onClick={(e,d) => this.toggleLassoSelection(lS.id)} style={{opacity: lS.selected ? 1 : .5 }}/>
-                            <Icon name='trash' style={{display: 'inline'}} onClick={(e,d) => this.removeLassoSelection(lS.id)} />
-                            <Icon name='download' style={{display: 'inline'}} onClick={(e,d) => this.downloadLassoSelection(lS.id)} />
-                        </Grid.Column>
-                    </Grid>
-                )
-            }))
-        }
+		let lassoSelections = () => {
+			if (this.state.lassoSelections.length == 0) {
+				return (
+					<Grid.Column>No user's lasso selections</Grid.Column>
+				);
+			}
 
-        return lassoSelections();
-    }
+			return (this.state.lassoSelections.map((lS, i) => {
+				return (
+					<Grid.Row columns={3} key={i}>
+						<Grid.Column>
+							{"Selection "+ lS.id}
+						</Grid.Column>
+						<Grid.Column>
+							<Input
+								size='mini'
+								style={{width: 75, height: 10}}
+								label={{ style: {backgroundColor: '#'+lS.color } }}
+								labelPosition='right'
+								placeholder={'#'+lS.color}
+							/>
+						</Grid.Column>
+						<Grid.Column>
+							<Icon name='eye' title='toggle show/hide selection' style={{display: 'inline'}} onClick={(e,d) => this.toggleLassoSelection(lS.id)} style={{opacity: lS.selected ? 1 : .5 }} className="pointer" />
+							&nbsp;
+							<Icon name='trash' title='remove this selection' style={{display: 'inline'}} onClick={(e,d) => this.removeLassoSelection(lS.id)} className="pointer"  />
+							&nbsp;
+							<Metadata selectionId={i} />
+						</Grid.Column>
+					</Grid.Row>
+				)
+			}))
+		}
 
-    componentWillMount() {
-        BackendAPI.onViewerSelectionsChange(this.selectionsListener);
-        BackendAPI.onActiveFeaturesChange(this.state.activePage, this.featuresListener);
-    }
+		return (
+			<Grid>
+				{lassoSelections()}
+			</Grid>
+		);
+	}
 
-    componentWillUnmount() {
-        BackendAPI.removeViewerSelectionsChange(this.selectionsListener);
-        BackendAPI.removeActiveFeaturesChange(this.state.activePage, this.featuresListener);
-    }
+	componentWillMount() {
+		BackendAPI.onViewerSelectionsChange(this.selectionsListener);
+	}
 
-    toggleLassoSelection(id) {
-        BackendAPI.toggleLassoSelection(id);
-    }
+	componentWillUnmount() {
+		BackendAPI.removeViewerSelectionsChange(this.selectionsListener);
+	}
 
-    removeLassoSelection(id) {
-        BackendAPI.removeViewerSelection(id);
-    }
+	toggleLassoSelection(id) {
+		BackendAPI.toggleLassoSelection(id);
+	}
 
-    getMetadata(selections) {
-        let settings = BackendAPI.getSettings();
-        let loomFilePath = BackendAPI.getActiveLoom();
-        let coordinates = BackendAPI.getActiveCoordinates();
-        let features = BackendAPI.getActiveFeatures();
-        let selectedGenes = [];
-        let selectedRegulons = [];
-        let selectedClusters = [];
-        let metadata = BackendAPI.getActiveLoomMetadata();
-        features.map(f => {
-            if (f.featureType == 'gene') selectedGenes.push(f.feature);
-            if (f.featureType == 'regulon') selectedRegulons.push(f.feature);
-            if (f.featureType.indexOf('Clustering:') == 0) {
-                metadata.cellMetaData.clusterings.map( clustering => {
-                    if (f.featureType.indexOf(clustering.name) != -1) {
-                        clustering.clusters.map(c => {
-                            if (c.description == f.feature) {
-                                selectedClusters.push({clusteringID: clustering.id, clusterID: c.id});
-                            }
-                        })                        
-                    }
-                })
-            }
-        })
-        selectedClusters.map(c => {
-            let query = {
-                loomFilePath: loomFilePath,
-                clusteringID: c.clusteringID,
-                clusterID: c.clusterID
-            }
-            console.log('getMarkerGenes', query);
-            BackendAPI.getConnection().then((gbc) => {
-                gbc.services.scope.Main.getMarkerGenes(query, (err, response) => {
-                    console.log('getMarkerGenes', response);
-                });
-            });
-        })
-        selections.map(s => {
-            let query = {
-                loomFilePath: loomFilePath,
-                cellIndices: s.points,
-                hasLogTranform: settings.hasLogTransform,
-                hasCpmTranform: settings.hasCpmNormalization,
-                selectedGenes: selectedGenes,    // List of genes to return epxression values for
-                selectedRegulons: selectedRegulons, // As above, for regulons and AUC values
-                clusterings: [], // IDs of clustering values to return per cell
-                annotations: []  // String name of annotations to return (from global metadata)
-            }
-            console.log('getCellMetaData', query);
-            BackendAPI.getConnection().then((gbc) => {
-                gbc.services.scope.Main.getCellMetaData(query, (err, response) => {
-                    console.log('getCellMetaData', response);
-                });
-            });
-        });
-    }
-
+	removeLassoSelection(id) {
+		BackendAPI.removeViewerSelection(id);
+	}
 }
