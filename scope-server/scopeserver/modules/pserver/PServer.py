97a3d8cf997f30eeae9f8ba45c954900bd60d338
M.D.W <mdewaegeneer@gmail.com>
2018-03-13 23:51:31 +0100

    Depending on the file type, the uploaded file is saved in a different data directory.

--- scope-server/scopeserver/modules/pserver/PServer.py
+++ scope-server/scopeserver/modules/pserver/PServer.py
@@ -13,6 +13,8 @@ import tempfile
 from http import server as httpserver
 import socketserver
 from urllib import parse as urllibparse
+from scopeserver.modules.gserver import GServer
+
 unicode = str
 
 def _decode_str_if_py2(inputstr, encoding='utf-8'):
@@ -211,12 +213,19 @@ class HTTPUploadHandler(httpserver.BaseHTTPRequestHandler):
         "Standard method to override in this Server object."
         try:
             self.log_message("Started file transfer")
-            # -- Save file (numbered to avoid overwriting, ex: foo-3.png)
             form = DroopyFieldStorage(fp=self.rfile,
                                       directory=self.directory,
                                       headers=self.headers,
                                       environ={'REQUEST_METHOD': self.command})
+            if form.getvalue('file-type') in GServer.data_dirs.keys():
+                self.directory = GServer.SCope.get_data_dir_path_by_file_type(file_type=form.getvalue('file-type'))
+            else:
+                self.send_error(415, "Unsupported file tyype")
+            # Update the directory of DroopyFieldStorage
+            form.directory = self.directory
+            print("Saving uploaded file in "+ self.directory)
             file_items = form[self.form_field]
+
             #-- Handle multiple file upload
             if not isinstance(file_items, list):
                 file_items = [file_items]
