415a36043616f09a5f1ff440752ee526b54dfe91
KrisDavie <kristofer.davie@kuleuven.vib.be>
2018-03-20 18:32:15 +0100

    Colour ordering fix

--- scope-server/scopeserver/modules/gserver/GServer.py
+++ scope-server/scopeserver/modules/gserver/GServer.py
@@ -607,7 +607,7 @@ class SCope(s_pb2_grpc.MainServicer):
             else:
                 features.append([_LOWER_LIMIT_RGB for n in range(n_cells)])
         if len(features) > 0 and len(hex_vec) == 0:
-            hex_vec = ["{0:02x}{0:02x}{0:02x}".format(_NO_EXPR_RGB) if r == g == b == 0
+            hex_vec = ["null" if r == g == b == 0
                        else "{0:02x}{1:02x}{2:02x}".format(int(r), int(g), int(b))
                        for r, g, b in zip(features[0], features[1], features[2])]
 
@@ -889,7 +889,7 @@ class GeneSetEnrichment:
             vmax[0] = self.scope.getVmax(aucs)
             vals = aucs / vmax[0]
             vals = (((_UPPER_LIMIT_RGB - _LOWER_LIMIT_RGB) * (vals - min(vals))) / (1 - min(vals))) + _LOWER_LIMIT_RGB
-            hex_vec = ["{0:02x}{0:02x}{0:02x}".format(_NO_EXPR_RGB) if r == g == b == 0
+            hex_vec = ["null" if r == g == b == 0
                        else "{0:02x}{1:02x}{2:02x}".format(int(r), int(g), int(b))
                        for r, g, b in zip(vals, np.zeros(len(aucs)), np.zeros(len(aucs)))]
             return s_pb2.GeneSetEnrichmentReply(progress=s_pb2.Progress(value=state.get_step(), status=state.get_status_message()),
