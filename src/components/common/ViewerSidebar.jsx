1df6c7e25488170f1bb8fad41c66342b90f68f87
mase5 <mdewaegeneer@gmail.com>
2018-06-07 17:15:21 +0200

    Fix ViewerSideBar bug not inside screen when AppSideBar is visible. Cell selection input color is disabled by default.

--- src/components/common/ViewerSidebar.jsx
+++ src/components/common/ViewerSidebar.jsx
@@ -55,10 +55,11 @@ class ViewerSidebar extends Component {
 						<Grid.Column>
 							<Input
 								size='mini'
-								style={{width: 75, height: 10}}
+								style={{width: 75, height: 15}}
 								label={{ style: {backgroundColor: '#'+lS.color } }}
 								labelPosition='right'
 								placeholder={'#'+lS.color}
+								disabled
 							/>
 						</Grid.Column>
 						<Grid.Column>
@@ -85,7 +86,8 @@ class ViewerSidebar extends Component {
 		}
 
 		let featureTab = (i) => {
-			let metadata = activeFeatures[i] && activeFeatures[i].feature ? "" : "Please use the inputs above to select a feature";
+			let colors = ["red", "green", "blue"]
+			let metadata = activeFeatures[i] && activeFeatures[i].feature ? "" : <div>No additional information shown for the feature queried in the <b style={{color: colors[i]}}>{colors[i]}</b> query box because it is empty. Additional information (e.g.: cluster markers, regulon motif, regulon target genes, ...) can be displayed here when querying clusters or regulons.<br/><br/></div>;
 
 			if (activeFeatures[i] && activeFeatures[i].metadata) {
 				let md = activeFeatures[i].metadata
