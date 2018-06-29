40a630b6a55977f07e3304bf87da846b0fb37ebc
Kristofer Davie <kristofer.davie@kuleuven.vib.be>
2018-06-29 15:26:09 +0200

    Fix more instances of hasattr

--- opt/setup.py
+++ opt/setup.py
@@ -5,7 +5,7 @@ setup(name='scope-server',
                         'scope-server = scopeserver.dataserver:dev'
                         ]
                     },
-      version='1.0.1',
+      version='1.0.3',
       description='SCope Data Server: a server to load and serve the data to the SCope Client',
       url='',
       author='Maxime De Waegeneer',
