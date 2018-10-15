8e836b41c4e3fa9c25320990f2c27093c047a99c
Kristofer Davie <u0094113@kuleuven.be>
2018-10-15 16:39:51 +0200

    Implement check for correct python version

--- bin/install.js
+++ bin/install.js
@@ -2,5 +2,20 @@
 var shell = require("shelljs");
 console.log("Installing SCope Bind Server")
 shell.exec('npm install opt/scopeserver/bindserver');
+
+var execSync = require('child_process').execSync;
+try {
+    var output = execSync('python3 -c "import sys; print(sys.version)"', { encoding: 'utf-8' });
+    var python = 'python3'
+} catch(e) {
+    var output = execSync('python -c "import sys; print(sys.version)"', { encoding: 'utf-8' });
+    var python = 'python'
+} finally {
+    console.log(output);
+    if (!(/^3.[0-9]*.[0-9]*.*/.test(output))) {
+        throw("Compatible python version not found!")
+    }
+}
+
 console.log("Installing SCope Data Server...")
-shell.exec('cd opt && python3 setup.py develop')
+shell.exec('cd opt && ' + python + ' setup.py develop')
