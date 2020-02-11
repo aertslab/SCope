a383b7ba57dfb4218513a7cd3bcf27320992f4c4
James Collier <james.collier412@gmail.com>
2020-02-11 15:56:46 +0100

    Check for Python 3.6 - 3.8

--- bin/install.js
+++ bin/install.js
@@ -6,13 +6,18 @@ shell.exec('npm install');
 shell.cd('../../../');
 var commandExists = require('command-exists');
 
-commandExists('poetry')
-    .then(() => {
-        console.log('Installing SCope Data Server...');
-        shell.exec('cd opt && poetry install');
-    })
-    .catch(function() {
-        console.log(
-            'The poetry program does not exist. Abort. Please install it.'
-        );
-    });
+try {
+  var output = execSync('python3 -c "import sys; print(sys.version)"', { encoding: 'utf-8' });
+  var python = 'python3'
+} catch(e) {
+    var output = execSync('python -c "import sys; print(sys.version)"', { encoding: 'utf-8' });
+    var python = 'python'
+} finally {
+    console.log(output);
+    if (!(/^3.[6-8].[0-9].*/.test(output))) {
+        throw("Compatible python version not found!")
+   }
+}
+
+console.log("Installing SCope Data Server...")
+shell.exec('cd opt && ' + python + ' poetry install')
