54c60ac0229be5ad4a062f95f5242a9b59b3d30a
KrisDavie <kristofer.davie@kuleuven.vib.be>
2020-02-21 13:21:47 +0100

    Lint and unit test fixes

--- opt/test/test_scopeserver.py
+++ opt/test/test_scopeserver.py
@@ -1,7 +1,8 @@
 import scopeserver.dataserver.modules.gserver.GServer as gs
 
 def test_vmax():
-    scope = gs.SCope()
+    config = {}
+    scope = gs.SCope(config)
     vals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9]
     assert scope.get_vmax(vals) == (9.0, 9)
 
