be9d6d54f0f45a2dbacd272d581c388c98b726ca
James Collier <james.collier@vib.be>
2020-02-24 22:15:49 +0100

    Start using typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import 'semantic-ui-css/semantic.min.css';
 import './css/header.css';
@@ -7,24 +7,24 @@ import './css/sidebar.css';
 import './css/viewer.css';
 import './css/features.css';
 import { HashRouter, Route } from 'react-router-dom';
-import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from 'react-hot-loader'
 import { CookiesProvider } from 'react-cookie';
 
 const renderApp = () => {
-    ReactDOM.render(
+    ReactDOM.render((
         <AppContainer>
             <CookiesProvider>
                 <HashRouter>
-                    <Route path='/:uuid?/:loom?/:page?' component={App} />
+                    <Route path="/:uuid?/:loom?/:page?" component={App} />
                 </HashRouter>
             </CookiesProvider>
-        </AppContainer>,
-        document.getElementById('scope')
-    );
-};
-
-if (module.hot) {
-    module.hot.accept('./components/App', renderApp);
+        </AppContainer>
+    ), document.getElementById('scope'));
 }
 
-renderApp();
+if(module.hot) {
+    module.hot.accept('./components/App', renderApp)
+}
+
+renderApp()
+
