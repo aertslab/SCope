70195c53275b47cb8845bb40f4623bcfcc046d93
James Collier <james.collier@vib.be>
2020-02-25 14:43:01 +0100

    Automatic fixes

--- src/main.tsx
+++ src/main.tsx
@@ -7,24 +7,24 @@ import './css/sidebar.css';
 import './css/viewer.css';
 import './css/features.css';
 import { HashRouter, Route } from 'react-router-dom';
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader';
 import { CookiesProvider } from 'react-cookie';
 
 const renderApp = () => {
-    render((
-        <AppContainer>
-            <CookiesProvider>
-                <HashRouter>
-                    <Route path="/:uuid?/:loom?/:page?" component={App} />
-                </HashRouter>
-            </CookiesProvider>
-        </AppContainer>
-    ), document.getElementById('scope'));
-}
+  render(
+    <AppContainer>
+      <CookiesProvider>
+        <HashRouter>
+          <Route path='/:uuid?/:loom?/:page?' component={App} />
+        </HashRouter>
+      </CookiesProvider>
+    </AppContainer>,
+    document.getElementById('scope')
+  );
+};
 
-if(module.hot) {
-    module.hot.accept('./components/App', renderApp)
+if (module.hot) {
+  module.hot.accept('./components/App', renderApp);
 }
 
 renderApp();
-
