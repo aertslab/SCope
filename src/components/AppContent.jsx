97320184a9702bf68dda80c55e1e5d2396442908
James Collier <jhc-at-home@users.noreply.github.com>
2020-02-26 13:26:08 +0100

    Re-run prettier with a tabWidth of 4 (#261)

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -9,17 +9,17 @@ import Compare from './pages/Compare';
 import Tutorial from './pages/Tutorial';
 
 export default class AppContent extends Component {
-  render() {
-    return (
-      <Segment style={{ height: window.innerHeight - 85 + 'px' }}>
-        {/* TODO: remove css calculations */}
-        <Route path='/:uuid/:loom?/welcome' component={Welcome} />
-        <Route path='/:uuid/:loom?/dataset' component={Dataset} />
-        <Route path='/:uuid/:loom?/gene' component={Expression} />
-        <Route path='/:uuid/:loom?/regulon' component={Regulon} />
-        <Route path='/:uuid/:loom?/compare' component={Compare} />
-        <Route path='/:uuid/:loom?/tutorial' component={Tutorial} />
-      </Segment>
-    );
-  }
+    render() {
+        return (
+            <Segment style={{ height: window.innerHeight - 85 + 'px' }}>
+                {/* TODO: remove css calculations */}
+                <Route path='/:uuid/:loom?/welcome' component={Welcome} />
+                <Route path='/:uuid/:loom?/dataset' component={Dataset} />
+                <Route path='/:uuid/:loom?/gene' component={Expression} />
+                <Route path='/:uuid/:loom?/regulon' component={Regulon} />
+                <Route path='/:uuid/:loom?/compare' component={Compare} />
+                <Route path='/:uuid/:loom?/tutorial' component={Tutorial} />
+            </Segment>
+        );
+    }
 }
