dbde0b40a3299d1f0e0733432055f0f78f0cbe55
mase5 <mdewaegeneer@gmail.com>
2020-02-28 13:13:39 +0100

    Prettify bin/*.js and index.js

--- bin/configure.js
+++ bin/configure.js
@@ -1,6 +1,6 @@
-console.log("Configuring SCope...")
+console.log('Configuring SCope...');
 const makeDir = require('make-dir');
-makeDir('assets').then(path => {
-    console.log(path + " directory created.");
+makeDir('assets').then((path) => {
+    console.log(path + ' directory created.');
     console.log("SCope ready for 'npm run dev' or 'npm run build'");
 });
--- bin/install.js
+++ bin/install.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
-var shell = require("shelljs");
-console.log("Installing SCope Bind Server")
-shell.cd('opt/scopeserver/bindserver')
+var shell = require('shelljs');
+console.log('Installing SCope Bind Server');
+shell.cd('opt/scopeserver/bindserver');
 shell.exec('npm install');
-shell.cd('../../../')
+shell.cd('../../../');
 var commandExists = require('command-exists');
 
-
-commandExists('poetry').then(function (command) {
-  console.log("Installing SCope Data Server...")
-  shell.exec('cd opt && poetry install')
-}).catch(function () {
-  console.log("The poetry program does not exist. Abort. Please install it.")
-});
+commandExists('poetry')
+    .then(() => {
+        console.log('Installing SCope Data Server...');
+        shell.exec('cd opt && poetry install');
+    })
+    .catch(function() {
+        console.log(
+            'The poetry program does not exist. Abort. Please install it.'
+        );
+    });
