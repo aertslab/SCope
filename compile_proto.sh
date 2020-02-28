60ca52d7603be97ae43fa3380d659d026f20885a
mase5 <mdewaegeneer@gmail.com>
2020-02-28 13:03:11 +0100

    Blacked and add pylint off and ignore pb2 files

--- compile_proto.sh
+++ compile_proto.sh
@@ -6,7 +6,7 @@ B="from scopeserver.dataserver.modules.gserver import s_pb2 as s__pb2"
 # Update the import
 sed -i -e "s#$A#$B#g" "$GSERVER_DIR/s_pb2_grpc.py"
 # Do no pylint & ignore type s_pb2.py
-sed -i '1s/^/# type: ignore\n/' "$GSERVER_DIR/s_pb2_grpc.py"
-sed -i '1s/^/# pylint: skip-file\n/' "$GSERVER_DIR/s_pb2_grpc.py"
-# Do no pylint s_pb2.py
+sed -i '1s/^/# type: ignore\n/' "$GSERVER_DIR/s_pb2.py"
 sed -i '1s/^/# pylint: skip-file\n/' "$GSERVER_DIR/s_pb2.py"
+# Do no pylint s_pb2.py
+sed -i '1s/^/# pylint: skip-file\n/' "$GSERVER_DIR/s_pb2_grpc.py"
