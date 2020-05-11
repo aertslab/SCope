f8b05330794e84f23d45b96b22804478f238688e
James Collier <james.collier@vib.be>
2020-05-11 14:43:06 +0200

    Change default for "app_mode" config to False

---
+++ opt/scopeserver/scope/config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+""" Server configuration handling. """
+
+from typing import Dict, Optional, Union
+from pathlib import Path
+import json
+
+
+def defaults() -> Dict[str, Union[str, int, bool]]:
+    """ Default configuration values. """
+    return {
+        "app_mode": False,
+        "debug": True,
+        "pPort": 55851,
+        "xPort": 55852,
+        "gPort": 55853,
+    }
+
+
+def from_file(config_filename: Optional[Union[Path, str]]) -> Dict[str, Union[str, int, bool]]:
+    """ Read configuration from a provided file.
+
+    If the configuration filename is not provided. Returns default values.
+    """
+
+    config = defaults()
+    if config_filename is not None:
+        with open(config_filename) as config_file:
+            config = {**config, **json.loads(config_file.read())}
+
+    return config
--- opt/test/test_config.py
+++ opt/test/test_config.py
@@ -1,47 +1,32 @@
-"""
-Tests for the scopeserver.config module
-"""
-
-from json.decoder import JSONDecodeError
-from pathlib import Path, PureWindowsPath
+from pathlib import Path
 
 import pytest
+from json.decoder import JSONDecodeError
 
-import scopeserver.config as config
+import scopeserver.scope.config as config
 
 
 def test_defaults():
     cfg = config.defaults()
 
-    assert cfg["app_mode"]
+    assert cfg["app_mode"] == False
     assert cfg["debug"]
     assert cfg["pPort"] == 55851
     assert cfg["xPort"] == 55852
     assert cfg["gPort"] == 55853
-    assert len(cfg["dataHashSecret"]) == 64 and not cfg["dataHashSecret"].isspace()
-    assert Path(cfg["data"]) == Path("data")
 
 
 def test_load_from_file():
     cfg = config.from_file(Path("test") / Path("data") / Path("config.json"))
 
     # Check unchanged defaults are the same
-    assert cfg["app_mode"]
-    assert not cfg["debug"]
+    assert cfg["app_mode"] == False
     assert cfg["pPort"] == 55851
     assert cfg["xPort"] == 55852
     assert cfg["gPort"] == 55853
-    assert len(cfg["dataHashSecret"]) == 64 and not cfg["dataHashSecret"].isspace()
-    assert PureWindowsPath(cfg["data"]) == PureWindowsPath("C:/User/SCope data")
 
     # Check new values
     assert cfg["extra setting"] == "value"
-
-
-def test_blank_dataHashSecret():
-    cfg = config.from_string('{"dataHashSecret": "    "}')
-
-    assert len(cfg["dataHashSecret"]) == 64 and not cfg["dataHashSecret"].isspace()
 
 
 def test_load_from_nonexistant_file():
