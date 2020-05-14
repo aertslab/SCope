3afb749eff784e685e9608091e6d4e604c8cc91c
James Collier <james.collier@vib.be>
2020-05-14 10:45:39 +0200

    Revert "Change default for "app_mode" config to False"
    This reverts commit a8968979e866084376ecb63b02972ca5291a44b3.

--- opt/test/test_config.py
+++ opt/test/test_config.py
@@ -1,32 +1,47 @@
-from pathlib import Path
+"""
+Tests for the scopeserver.config module
+"""
+
+from json.decoder import JSONDecodeError
+from pathlib import Path, PureWindowsPath
 
 import pytest
-from json.decoder import JSONDecodeError
 
-import scopeserver.scope.config as config
+import scopeserver.config as config
 
 
 def test_defaults():
     cfg = config.defaults()
 
-    assert not cfg["app_mode"]
+    assert cfg["app_mode"]
     assert cfg["debug"]
     assert cfg["pPort"] == 55851
     assert cfg["xPort"] == 55852
     assert cfg["gPort"] == 55853
+    assert len(cfg["dataHashSecret"]) == 64 and not cfg["dataHashSecret"].isspace()
+    assert Path(cfg["data"]) == Path("data")
 
 
 def test_load_from_file():
     cfg = config.from_file(Path("test") / Path("data") / Path("config.json"))
 
     # Check unchanged defaults are the same
-    assert not cfg["app_mode"]
+    assert cfg["app_mode"]
+    assert not cfg["debug"]
     assert cfg["pPort"] == 55851
     assert cfg["xPort"] == 55852
     assert cfg["gPort"] == 55853
+    assert len(cfg["dataHashSecret"]) == 64 and not cfg["dataHashSecret"].isspace()
+    assert PureWindowsPath(cfg["data"]) == PureWindowsPath("C:/User/SCope data")
 
     # Check new values
     assert cfg["extra setting"] == "value"
+
+
+def test_blank_dataHashSecret():
+    cfg = config.from_string('{"dataHashSecret": "    "}')
+
+    assert len(cfg["dataHashSecret"]) == 64 and not cfg["dataHashSecret"].isspace()
 
 
 def test_load_from_nonexistant_file():
