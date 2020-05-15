0310f43cf2cac43d65407a2e0d165b9a6df2a2e2
KrisDavie <kristofer.davie@kuleuven.vib.be>
2020-05-15 12:22:13 +0200

    Fix tests

--- opt/test/test_cell_color_by_features.py
+++ opt/test/test_cell_color_by_features.py
@@ -6,11 +6,15 @@ def test_normalise_vals():
     vals = np.array([0, 2, 1, 4, 5, 0, 7, 8, 9, 10])
     v_max = 8
     v_min = 2.1
-    assert CellColorByFeatures.normalise_vals(vals, v_max, v_min) == [0, 2, 2, 74, 112, 0, 188, 225, 225, 225]
+    np.testing.assert_equal(
+        CellColorByFeatures.normalise_vals(vals, v_max, v_min), np.array([0, 2, 2, 74, 112, 0, 188, 225, 225, 225])
+    )
 
 
 def test_normalise_vals_low_vmax():
     vals = np.array([0, 2, 1, 4, 5, 0, 7, 8, 9, 10])
     v_max = 0.5
     v_min = 0
-    assert CellColorByFeatures.normalise_vals(vals, v_max, v_min) == [0, 255, 255, 255, 255, 0, 255, 225, 225, 225]
+    np.testing.assert_equal(
+        CellColorByFeatures.normalise_vals(vals, v_max, v_min), np.array([0, 225, 225, 225, 225, 0, 225, 225, 225, 225])
+    )
