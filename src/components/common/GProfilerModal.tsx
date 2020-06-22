739d96385a4700e9b1ed25fea44322d815bdd8c6
dweemx <mdewaegeneer@gmail.com>
2020-06-22 12:25:33 +0200

    Add organism dropdown and token input as CC

--- src/components/common/GProfilerModal.tsx
+++ src/components/common/GProfilerModal.tsx
@@ -3,13 +3,23 @@ import { BackendAPI } from './API';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import React, { Component } from 'react';
-import { Button, Modal, Form, Checkbox, Table } from 'semantic-ui-react';
+import {
+    Button,
+    Modal,
+    Form,
+    Checkbox,
+    Table,
+    Input,
+    Select,
+} from 'semantic-ui-react';
 import { instanceOf } from 'prop-types';
-import { withCookies, Cookies, ReactCookieProps } from 'react-cookie';
+import { withCookies, Cookies } from 'react-cookie';
 
 interface IGProfilerPopupState {
     showModal: boolean;
     topNumFeatures: number[];
+    selectedOrganism: string;
+    gProfilerToken: string;
 }
 
 interface IGProfilerPopupProps {
@@ -32,6 +42,8 @@ class GProfilerPopup extends Component<
         this.state = {
             showModal: false,
             topNumFeatures: [],
+            selectedOrganism: null,
+            gProfilerToken: null,
         };
     }
 
@@ -76,7 +88,19 @@ class GProfilerPopup extends Component<
             };
         };
 
-        console.log(this.state.topNumFeatures);
+        const organisms = [
+            { key: 1, text: 'Human', value: 'hsapiens' },
+            { key: 2, text: 'Mouse', value: 'mmusculus' },
+            { key: 3, text: 'Drosophila melanogaster', value: 'dmelanogaster' },
+        ];
+
+        const handleSelectOrganism = (e, { value }) => {
+            this.setState({ selectedOrganism: value });
+        };
+
+        const handleChangeToken = (e, { value }) => {
+            this.setState({ gProfilerToken: value });
+        };
 
         return (
             <>
@@ -109,80 +133,98 @@ class GProfilerPopup extends Component<
                                 Totral number of features:
                                 {this.props.numFeatures}
                             </div>
-                            <Table compact celled definition>
-                                <Table.Header>
-                                    <Table.Row>
-                                        <Table.HeaderCell />
-                                        <Table.HeaderCell>
-                                            Gene List with Number of Top
-                                            Features to Use
-                                        </Table.HeaderCell>
-                                    </Table.Row>
-                                </Table.Header>
-                                <Table.Body>
-                                    {topNumFeaturesArray.map(
-                                        (
-                                            topNumFeaturesValue,
-                                            topNumFeaturesIndex
-                                        ) => {
-                                            const handleToggleTopNumFeatures = () => {
-                                                if (
-                                                    this.state.topNumFeatures.includes(
-                                                        topNumFeaturesValue
-                                                    )
-                                                ) {
-                                                    this.setState({
-                                                        topNumFeatures: this.state.topNumFeatures.filter(
-                                                            (value) =>
-                                                                value !=
-                                                                topNumFeaturesValue
-                                                        ),
-                                                    });
-                                                } else {
-                                                    this.setState({
-                                                        topNumFeatures: [
-                                                            ...this.state
-                                                                .topNumFeatures,
-                                                            topNumFeaturesValue,
-                                                        ],
-                                                    });
-                                                }
-                                            };
+                            <Form>
+                                <Table compact celled definition>
+                                    <Table.Header>
+                                        <Table.Row>
+                                            <Table.HeaderCell />
+                                            <Table.HeaderCell>
+                                                Gene List with Number of Top
+                                                Features to Use
+                                            </Table.HeaderCell>
+                                        </Table.Row>
+                                    </Table.Header>
+                                    <Table.Body>
+                                        {topNumFeaturesArray.map(
+                                            (
+                                                topNumFeaturesValue,
+                                                topNumFeaturesIndex
+                                            ) => {
+                                                const handleToggleTopNumFeatures = () => {
+                                                    if (
+                                                        this.state.topNumFeatures.includes(
+                                                            topNumFeaturesValue
+                                                        )
+                                                    ) {
+                                                        this.setState({
+                                                            topNumFeatures: this.state.topNumFeatures.filter(
+                                                                (value) =>
+                                                                    value !=
+                                                                    topNumFeaturesValue
+                                                            ),
+                                                        });
+                                                    } else {
+                                                        this.setState({
+                                                            topNumFeatures: [
+                                                                ...this.state
+                                                                    .topNumFeatures,
+                                                                topNumFeaturesValue,
+                                                            ],
+                                                        });
+                                                    }
+                                                };
 
-                                            const isSelected = this.state.topNumFeatures.includes(
-                                                topNumFeaturesValue
-                                            );
+                                                const isSelected = this.state.topNumFeatures.includes(
+                                                    topNumFeaturesValue
+                                                );
 
-                                            return (
-                                                <Table.Row>
-                                                    <Table.Cell collapsing>
-                                                        <Checkbox
-                                                            onChange={
-                                                                handleToggleTopNumFeatures
-                                                            }
-                                                        />
-                                                    </Table.Cell>
-                                                    <Table.Cell>
-                                                        {isSelected ? (
-                                                            <b>
-                                                                {`Top ${topNumFeaturesValue}`}
-                                                            </b>
-                                                        ) : (
-                                                            <span
-                                                                style={{
-                                                                    textDecorationLine:
-                                                                        'line-through',
-                                                                    textDecorationStyle:
-                                                                        'solid',
-                                                                }}>{`Top ${topNumFeaturesValue}`}</span>
-                                                        )}
-                                                    </Table.Cell>
-                                                </Table.Row>
-                                            );
-                                        }
-                                    )}
-                                </Table.Body>
-                            </Table>
+                                                return (
+                                                    <Table.Row>
+                                                        <Table.Cell collapsing>
+                                                            <Checkbox
+                                                                onChange={
+                                                                    handleToggleTopNumFeatures
+                                                                }
+                                                            />
+                                                        </Table.Cell>
+                                                        <Table.Cell>
+                                                            {isSelected ? (
+                                                                <b>
+                                                                    {`Top ${topNumFeaturesValue}`}
+                                                                </b>
+                                                            ) : (
+                                                                <span
+                                                                    style={{
+                                                                        textDecorationLine:
+                                                                            'line-through',
+                                                                        textDecorationStyle:
+                                                                            'solid',
+                                                                    }}>{`Top ${topNumFeaturesValue}`}</span>
+                                                            )}
+                                                        </Table.Cell>
+                                                    </Table.Row>
+                                                );
+                                            }
+                                        )}
+                                    </Table.Body>
+                                </Table>
+                                <Form.Group widths='equal'>
+                                    <Form.Field
+                                        control={Select}
+                                        label='Organism'
+                                        options={organisms}
+                                        placeholder='Choose an organism'
+                                        onChange={handleSelectOrganism}
+                                    />
+                                    <Form.Field
+                                        control={Input}
+                                        label='g:Profiler Token (Optional)'
+                                        placeholder='Token'
+                                        value={this.state.gProfilerToken}
+                                        onChange={handleChangeToken}
+                                    />
+                                </Form.Group>
+                            </Form>
                         </Modal.Description>
                     </Modal.Content>
                     <Modal.Actions>
