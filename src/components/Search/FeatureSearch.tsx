c0cf52a250ca0624615d38f817522b5f55a71be4
James Collier <james.collier@vib.be>
2020-08-13 16:49:05 +0200

    Simplify and organise the FeatureSearchBox component
    * Move to typescript
    * Remove unnecessary and unused code
    * Upgrade semantic-ui-react to a more recent version
    * Re-organise some dependant code
    * Update packages and install immer

---
+++ src/components/Search/FeatureSearch.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+import { FeatureType } from '../../features';
+
+import { FeatureSearchBox } from './FeatureSearchBox';
+
+interface FeatureSearchProps {
+    /** Use this to initialise the feature types dropdown */
+    feature: FeatureType;
+    /** Use this to restrict selectable feature types (to only `feature`) */
+    singleFeature: boolean;
+}
+
+/**
+ * A series of 3 text search input for properties on a dataset
+ */
+export function FeatureSearch(props: FeatureSearchProps) {
+    return (
+        <React.Fragment>
+            <Grid.Row columns='4' centered>
+                {[0, 1, 2].map((i) => (
+                    <Grid.Column key={i}>
+                        <FeatureSearchBox
+                            field={i}
+                            type={props.feature}
+                            singleFeature={props.singleFeature}
+                            enabled={true}
+                        />
+                    </Grid.Column>
+                ))}
+                <Grid.Column />
+            </Grid.Row>
+        </React.Fragment>
+    );
+}
