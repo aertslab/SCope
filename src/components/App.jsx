72531f152384c655ff45f78fdfdbf5b8ea40f857
James Collier <james.collier@vib.be>
2020-08-27 08:48:52 +0200

    Update dependencies and fix cookie warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -72,10 +72,15 @@ class App extends Component {
 
     acceptCookies() {
         this.props.cookies.set('CookieConsent', 'true');
+        this.setUUIDCookie();
+        this.setState({ cookiesAllowed: true });
+    }
+
+    setUUIDCookie() {
         this.props.cookies.set(cookieName, this.props.match.params.uuid, {
             path: '/',
+            sameSite: 'strict',
         });
-        this.setState({ cookiesAllowed: true });
     }
 
     render() {
@@ -304,7 +309,9 @@ class App extends Component {
 
         if (match.params.uuid) {
             if (match.params.uuid == 'permalink') {
-                if (DEBUG) console.log('Permalink detected');
+                if (DEBUG) {
+                    console.log('Permalink detected');
+                }
                 this.restoreSession(
                     ip,
                     cookies.get(cookieName),
@@ -317,18 +324,20 @@ class App extends Component {
                     match.params.loom
                 );
             } else {
-                if (DEBUG)
+                if (DEBUG) {
                     console.log('Params UUID detected:', match.params.uuid);
+                }
                 this.checkUUID(ip, match.params.uuid);
                 console.log(this.state.cookiesAllowed);
                 console.log(cookieName, match.params.uuid, { path: '/' });
                 if (this.state.cookiesAllowed) {
-                    cookies.set(cookieName, match.params.uuid, { path: '/' });
+                    this.setUUIDCookie();
                 }
             }
         } else if (cookies.get(cookieName)) {
-            if (DEBUG)
+            if (DEBUG) {
                 console.log('Cookie UUID detected:', cookies.get(cookieName));
+            }
             this.checkUUID(ip, cookies.get(cookieName));
         } else {
             if (DEBUG) console.log('No UUID detected');
@@ -369,13 +378,16 @@ class App extends Component {
                 gbc.ws.onclose = (err) => {
                     this.setState({ error: true });
                 };
-                if (DEBUG) console.log('request RemainingUUIDTime', query);
+                if (DEBUG) {
+                    console.log('request RemainingUUIDTime', query);
+                }
                 gbc.services.scope.Main.getRemainingUUIDTime(
                     query,
                     (err, response) => {
                         this.mouseClicks = 0;
-                        if (DEBUG)
+                        if (DEBUG) {
                             console.log('getRemainingUUIDTime', response);
+                        }
                         if (response.sessionsLimitReached) {
                             this.props.setAppLoading(false);
                             this.setState({
@@ -385,7 +397,7 @@ class App extends Component {
                             this.timeout = response
                                 ? parseInt(response.timeRemaining * 1000)
                                 : 0;
-                            // cookies.set(cookieName, uuid, { path: '/', maxAge: this.timeout });
+
                             if (!ping) {
                                 this.props.setAppLoading(false);
                                 this.props.setUUID(uuid);
@@ -396,8 +408,9 @@ class App extends Component {
                                 this.timer = setInterval(() => {
                                     this.timeout -= timer;
                                     if (this.timeout < 0) {
-                                        if (DEBUG)
+                                        if (DEBUG) {
                                             console.log('Session timed out');
+                                        }
                                         cookies.remove(cookieName);
                                         clearInterval(this.timer);
                                         this.timer = null;
@@ -406,11 +419,12 @@ class App extends Component {
                                         }
                                         this.forceUpdate();
                                     } else {
-                                        if (DEBUG)
+                                        if (DEBUG) {
                                             console.log(
                                                 'Session socket ping @ ',
                                                 this.timeout
                                             );
+                                        }
                                         this.checkUUID(ip, uuid, true);
                                     }
                                 }, timer);
