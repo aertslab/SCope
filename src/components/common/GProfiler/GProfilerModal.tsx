5c8422fb75c363528a3b69077fef79ba1138d8ae
dweemx <mdewaegeneer@gmail.com>
2020-08-28 14:58:24 +0200

    Extract all static logic out of GProfilerPopup component into utils.ts

--- src/components/common/GProfiler/GProfilerModal.tsx
+++ src/components/common/GProfiler/GProfilerModal.tsx
@@ -13,11 +13,9 @@ import {
     Dropdown,
 } from 'semantic-ui-react';
 
-import {
-    GPROFILER_API_ENDPOINT__AVAILABLE_ORGANISMS,
-    GPROFILER_API_ENDPOINT__GENE_LIST_FUNCTIONAL_ENRICHMENT,
-    GPROFILER_LINK_MAX_LENGTH,
-} from './constants';
+import { GPROFILER_API_ENDPOINT__AVAILABLE_ORGANISMS } from './constants';
+
+import { buildMetricTable, checkCreateGProfilerLink } from './utils';
 
 interface IGProfilerPopupState {
     error: string;
@@ -123,13 +121,13 @@ class GProfilerPopup extends Component<
                 };
             }
         );
-        this.featureMetricTable = this.buildMetricTable();
+        this.featureMetricTable = buildMetricTable(this.featureMetadata);
         this.onOpenModal.bind(this);
         this.onCloseModal.bind(this);
-        this.handleSelectOrganism.bind(this);
-        this.handleSelectSortBy.bind(this);
-        this.handleChangeToken.bind(this);
-        this.handleClickGotoGProfilerURL.bind(this);
+        this.onSelectOrganism.bind(this);
+        this.onSelectSortBy.bind(this);
+        this.onChangeToken.bind(this);
+        this.onClickGotoGProfilerURL.bind(this);
     }
 
     openModal = () => {
@@ -155,11 +153,54 @@ class GProfilerPopup extends Component<
         this.closeModal();
     };
 
+    onSelectGeneList = (geneListSize: number) => () => {
+        if (this.state.selectedTopGeneListsSizes.includes(geneListSize)) {
+            this.setState({
+                selectedTopGeneListsSizes: this.state.selectedTopGeneListsSizes.filter(
+                    (value) => value != geneListSize
+                ),
+            });
+        } else {
+            this.setState({
+                selectedTopGeneListsSizes: [
+                    ...this.state.selectedTopGeneListsSizes,
+                    geneListSize,
+                ],
+            });
+        }
+    };
+
+    onSelectOrganism = (e, { value }) => {
+        this.setState({ selectedOrganism: value });
+    };
+
+    onSelectSortBy = (e, { value }) => {
+        this.setState({ selectedSortBy: value });
+    };
+
+    onChangeToken = (e, { value }) => {
+        this.setState({
+            selectedOrganism: '',
+            gProfilerToken: value,
+        });
+    };
+
+    onClickGotoGProfilerURL = async () => {
+        const result = await checkCreateGProfilerLink({
+            featureMetricTable: this.featureMetricTable,
+            ...this.state,
+        });
+        if ('error' in result) this.setState({ error: result.error });
+
+        if (result.link === '' || typeof result.link !== 'string') return;
+        window.open(result.link);
+    };
+
     getNumFeatures = () => {
         return this.featureMetadata.genes.length;
     };
 
-    getTopNumFeaturesArray = () => {
+    getAvailableTopGeneListsSizes = () => {
         return [
             this.getNumFeatures() < 100 ? this.getNumFeatures() : 100,
             200,
@@ -169,158 +210,6 @@ class GProfilerPopup extends Component<
         ].filter((topNumFeaturesValue) =>
             topNumFeaturesValue <= this.getNumFeatures() ? true : false
         );
-    };
-
-    handleSelectOrganism = (e, { value }) => {
-        this.setState({ selectedOrganism: value });
-    };
-
-    handleSelectSortBy = (e, { value }) => {
-        this.setState({ selectedSortBy: value });
-    };
-
-    handleChangeToken = (e, { value }) => {
-        this.setState({
-            selectedOrganism: '',
-            gProfilerToken: value,
-        });
-    };
-
-    handleClickGotoGProfilerURL = async () => {
-        const gProfilerLink = await this.handleClickCreateGProfilerLink();
-        if (gProfilerLink === '' || typeof gProfilerLink !== 'string') return;
-        window.open(gProfilerLink);
-    };
-
-    buildMetricTable = () => {
-        return this.featureMetadata.genes.map((gene: string, idx: number) => {
-            return {
-                gene: gene,
-                ...this.featureMetadata.metrics.reduce(
-                    (accumalatedMetrics, metric) => ({
-                        ...accumalatedMetrics,
-                        [metric.accessor]: metric.values[idx],
-                    }),
-                    {}
-                ),
-            };
-        });
-    };
-
-    buildFeatureQuery = (topNumFeatures, sortedFeatureMetricTable) => {
-        return topNumFeatures
-            .map((topNumFeaturesElement) => {
-                return {
-                    numFeatures: topNumFeaturesElement,
-                    topSortedFeatures: sortedFeatureMetricTable
-                        .slice(0, topNumFeaturesElement)
-                        .reduce(
-                            (acc, sortedFeatureMetricTableRow) => [
-                                ...acc,
-                                sortedFeatureMetricTableRow['gene'],
-                            ],
-                            []
-                        )
-                        .join('\n'),
-                };
-            })
-            .reduce((acc, el) => {
-                return (
-                    acc +
-                    `>Top_${el.numFeatures}` +
-                    '\n' +
-                    el.topSortedFeatures +
-                    '\n'
-                );
-            }, '');
-    };
-
-    buildGProfilerLink = (organism: string, query: string) => {
-        const gProfilerQueryData = {
-            organism: organism,
-            query: query,
-            ordered: 'true',
-            all_results: 'false',
-            no_iea: 'false',
-            combined: 'true',
-            measure_underrepresentation: 'false',
-            domain_scope: 'annotated',
-            significance_threshold_method: 'g_SCS',
-            user_threshold: '0.05',
-            numeric_namespace: 'ENTREZGENE_ACC',
-            sources: 'GO:MF,GO:CC,GO:BP,KEGG,TF,REAC,MIRNA,HPA,CORUM,HP,WP',
-            background: '',
-        };
-
-        const gProfilerQueryString = Object.keys(gProfilerQueryData)
-            .map((key) => {
-                return (
-                    encodeURIComponent(key) +
-                    '=' +
-                    encodeURIComponent(gProfilerQueryData[key])
-                );
-            })
-            .join('&');
-
-        return `${GPROFILER_API_ENDPOINT__GENE_LIST_FUNCTIONAL_ENRICHMENT}?${gProfilerQueryString}`;
-    };
-
-    handleClickCreateGProfilerLink = async () => {
-        const {
-            topNumFeatures,
-            gProfilerToken,
-            selectedOrganism,
-            selectedSortBy,
-        } = this.state;
-        if (selectedSortBy === '') {
-            this.setState({
-                error: 'Please select a sort column',
-            });
-            return null;
-        }
-        if (gProfilerToken === '' && selectedOrganism === '') {
-            this.setState({
-                error: 'Please select an organism',
-            });
-            return null;
-        }
-
-        if (topNumFeatures.length == 0) {
-            this.setState({
-                error:
-                    'No gene list selected. At least one gene list is required.',
-            });
-            return null;
-        }
-        if (topNumFeatures.length == 0) {
-            this.setState({
-                error:
-                    'No gene list selected. At least one gene list is required.',
-            });
-            return null;
-        }
-
-        const sortedFeatureMetricTable = this.featureMetricTable.sort(
-            (a, b) => b[selectedSortBy] - a[selectedSortBy]
-        );
-        const topFeatureQuery = this.buildFeatureQuery(
-            topNumFeatures,
-            sortedFeatureMetricTable
-        );
-        const organism =
-            gProfilerToken !== null ? gProfilerToken : selectedOrganism;
-        const gProfilerLink = this.buildGProfilerLink(
-            organism,
-            topFeatureQuery
-        );
-        if (gProfilerLink.length > GPROFILER_LINK_MAX_LENGTH) {
-            this.setState({
-                error:
-                    'Too many genes in total. Try to select a combination of gene lists with fewer genes.',
-            });
-            return null;
-        }
-        return gProfilerLink;
     };
 
     setAvailableOrganisms(availableOrganisms) {
@@ -402,7 +291,7 @@ class GProfilerPopup extends Component<
                                     label='Sort Features By'
                                     options={this.availableSortBy}
                                     placeholder='Sort By'
-                                    onChange={this.handleSelectSortBy}
+                                    onChange={this.onSelectSortBy}
                                     value={selectedSortBy}
                                 />
                                 <Form.Field
@@ -412,7 +301,7 @@ class GProfilerPopup extends Component<
                                     label='Organism'
                                     options={this.state.availableOrganisms}
                                     placeholder='Choose an organism'
-                                    onChange={this.handleSelectOrganism}
+                                    onChange={this.onSelectOrganism}
                                     disabled={
                                         gProfilerToken !== null &&
                                         gProfilerToken !== ''
@@ -426,7 +315,7 @@ class GProfilerPopup extends Component<
                                     label='g:Profiler Token (Optional)'
                                     placeholder='Token'
                                     value={this.state.gProfilerToken}
-                                    onChange={this.handleChangeToken}
+                                    onChange={this.onChangeToken}
                                 />
                             </Form.Group>
                             {this.state.error !== '' && (
@@ -443,7 +332,7 @@ class GProfilerPopup extends Component<
                     <Button
                         type='button'
                         value='goto-gprofiler'
-                        onClick={this.handleClickGotoGProfilerURL}
+                        onClick={this.onClickGotoGProfilerURL}
                         primary>
                         {'Go to g:Profiler'}
                     </Button>
