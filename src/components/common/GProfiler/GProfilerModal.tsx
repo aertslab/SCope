fbde36465a361a556964e1009e8c6bfb153bf112
dweemx <mdewaegeneer@gmail.com>
2020-09-02 15:58:35 +0200

    Put TopGeneListsSelectionTable in its own file

--- src/components/common/GProfiler/GProfilerModal.tsx
+++ src/components/common/GProfiler/GProfilerModal.tsx
@@ -49,59 +49,6 @@ interface GProfilerPopupReduxProps {
     setError: (error: string) => void;
 }
 
-const TopGeneListsSelectionTable: React.FC<{
-    availableTopGeneListsSizes: number[];
-    selectedTopGeneListsSizes: number[];
-    onSelectGeneList: (geneListSize: number) => () => void;
-}> = ({
-    availableTopGeneListsSizes,
-    selectedTopGeneListsSizes,
-    onSelectGeneList,
-}) => {
-    return (
-        <Table compact celled definition>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell />
-                    <Table.HeaderCell>
-                        Gene List with Number of Top Features to Use
-                    </Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {availableTopGeneListsSizes.map((topGeneListsSize) => {
-                    const handleOnChangeGeneList = onSelectGeneList(
-                        topGeneListsSize
-                    );
-
-                    const isSelected = selectedTopGeneListsSizes.includes(
-                        topGeneListsSize
-                    );
-
-                    return (
-                        <Table.Row key={`tgls-${topGeneListsSize}`}>
-                            <Table.Cell collapsing>
-                                <Checkbox onChange={handleOnChangeGeneList} />
-                            </Table.Cell>
-                            <Table.Cell>
-                                {isSelected ? (
-                                    <b>{`Top ${topGeneListsSize}`}</b>
-                                ) : (
-                                    <span
-                                        style={{
-                                            textDecorationLine: 'line-through',
-                                            textDecorationStyle: 'solid',
-                                        }}>{`Top ${topGeneListsSize}`}</span>
-                                )}
-                            </Table.Cell>
-                        </Table.Row>
-                    );
-                })}
-            </Table.Body>
-        </Table>
-    );
-};
-
 const GProfilerPopup: React.FC<
     GProfilerPopupProps & GProfilerPopupReduxProps & RouteComponentProps
 > = (props) => {
