2529811247c2185ed99742647a0c51ddd8ca2684
James Collier <james.collier@vib.be>
2020-09-14 12:23:24 +0200

    Add Reducer type annotation (thanks @dweemx)

--- src/components/Search/reducer.ts
+++ src/components/Search/reducer.ts
@@ -1,5 +1,6 @@
 import produce from 'immer';
 import { has } from 'ramda';
+import { Reducer } from 'react-redux';
 
 import * as t from './actionTypes';
 import { State, init } from './model';
@@ -7,51 +8,54 @@ import { SearchAction } from './actions';
 
 const initialState: State = {};
 
-const reducer = produce((draft: State, action: SearchAction) => {
-    switch (action.type) {
-        case t.QUERY:
-            if (!has(action.payload.field, draft)) {
-                draft[action.payload.field] = init(action.payload.field);
-            }
+const reducer: Reducer<State, SearchAction> = produce(
+    (draft: State, action: SearchAction) => {
+        switch (action.type) {
+            case t.QUERY:
+                if (!has(action.payload.field, draft)) {
+                    draft[action.payload.field] = init(action.payload.field);
+                }
 
-            draft[action.payload.field].loading = true;
-            draft[action.payload.field].value = action.payload.query.query;
-            draft[action.payload.field].selected = undefined;
-            break;
+                draft[action.payload.field].loading = true;
+                draft[action.payload.field].value = action.payload.query.query;
+                draft[action.payload.field].selected = undefined;
+                break;
 
-        case t.RESULTS:
-            if (!has(action.payload.field, draft)) {
-                draft[action.payload.field] = init(action.payload.field);
-            }
+            case t.RESULTS:
+                if (!has(action.payload.field, draft)) {
+                    draft[action.payload.field] = init(action.payload.field);
+                }
 
-            draft[action.payload.field].loading = false;
-            draft[action.payload.field].results = action.payload.results;
-            break;
+                draft[action.payload.field].loading = false;
+                draft[action.payload.field].results = action.payload.results;
+                break;
 
-        case t.SELECT:
-            if (!has(action.payload.field, draft)) {
-                draft[action.payload.field] = init(action.payload.field);
-            }
+            case t.SELECT:
+                if (!has(action.payload.field, draft)) {
+                    draft[action.payload.field] = init(action.payload.field);
+                }
 
-            draft[action.payload.field].loading = false;
-            if (action.payload.selection !== undefined) {
-                draft[action.payload.field].value =
-                    action.payload.selection.title;
-            } else {
-                draft[action.payload.field].value = '';
-                draft[action.payload.field].results = [];
-            }
-            draft[action.payload.field].selected = action.payload.selection;
-            break;
+                draft[action.payload.field].loading = false;
+                if (action.payload.selection !== undefined) {
+                    draft[action.payload.field].value =
+                        action.payload.selection.title;
+                } else {
+                    draft[action.payload.field].value = '';
+                    draft[action.payload.field].results = [];
+                }
+                draft[action.payload.field].selected = action.payload.selection;
+                break;
 
-        case t.ERROR:
-            if (!has(action.payload.field, draft)) {
-                draft[action.payload.field] = init(action.payload.field);
-            }
+            case t.ERROR:
+                if (!has(action.payload.field, draft)) {
+                    draft[action.payload.field] = init(action.payload.field);
+                }
 
-            draft[action.payload.field].error = action.payload.message;
-            break;
-    }
-}, initialState);
+                draft[action.payload.field].error = action.payload.message;
+                break;
+        }
+    },
+    initialState
+);
 
 export { reducer };
