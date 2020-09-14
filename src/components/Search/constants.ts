b29e76d917f4b084f0d17c05a72b14fb84927948
James Collier <james.collier@vib.be>
2020-09-14 12:08:51 +0200

    Add basic Saga side-effect generation testing

--- src/components/Search/constants.ts
+++ src/components/Search/constants.ts
@@ -3,3 +3,5 @@ import { SemanticCOLORS } from 'semantic-ui-react';
 export const NAME = 'search';
 
 export const FEATURE_COLOURS: string[] = ['red', 'green', 'blue'];
+
+export const SEARCH_API_DELAY = 750; //ms
