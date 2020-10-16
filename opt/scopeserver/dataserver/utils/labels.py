52819504b35accac5d6e49fd495b68e22bb0fab7
James Collier <james.collier@vib.be>
2020-10-16 16:58:55 +0200

    Fix displaying incorrect colour annotation labels

--- opt/scopeserver/dataserver/utils/labels.py
+++ opt/scopeserver/dataserver/utils/labels.py
@@ -1,4 +1,4 @@
-from typing import List, NamedTuple, Iterator, Tuple, Generator
+from typing import List, NamedTuple, Iterator, Tuple, Generator, Iterable
 import logging
 from itertools import groupby
 
@@ -21,20 +21,37 @@ class FeatureLabel(NamedTuple):
     colour: str
     coordinate: Coordinate
 
+def uniq(data: Iterable[str]) -> List[str]:
+    seen = set()
+    result = []
+
+    for x in data:
+        if x not in seen:
+            seen.add(x)
+            result.append(x)
+
+    return result
 
 def label_annotation(loom: Loom, embedding: int, feature: str) -> List[FeatureLabel]:
-    """  """
-    annotations = groupby(sorted(loom.get_ca_attr_by_name(name=feature)))
+    """
+    Extract and group cells based on annotation. Place labels for each annotation
+    at the barycentre of the cell cluster.
+    """
+    values = loom.get_meta_data_annotation_by_name(name=feature)["values"]
+    all_annotations = loom.get_ca_attr_by_name(name=feature)
+    colours = uniq((constant.BIG_COLOR_LIST[values.index(i)] for i in all_annotations))
+
+    annotations = uniq(loom.get_ca_attr_by_name(name=feature))
 
     def labels() -> Generator[FeatureLabel, None, None]:
-        for i, (annotation, _) in enumerate(annotations):
+        for i, annotation in enumerate(annotations):
             coords = loom.get_coordinates(
                 coordinatesID=embedding, annotation=[Annotation(name=feature, values=[annotation])]
             )
 
             yield FeatureLabel(
                 label=annotation,
-                colour=constant.BIG_COLOR_LIST[i],
+                colour=colours[i],
                 coordinate=Coordinate(x=np.mean(coords["x"]), y=np.mean(coords["y"])),
             )
 
