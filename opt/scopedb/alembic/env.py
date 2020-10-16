a58c01208801f3dfacfcdf75e726567dadb7ecab
dweemx <mdewaegeneer@gmail.com>
2020-10-16 11:01:51 +0200

    Use pydantic BaseSettings to load env variables instead custom settings.py file

--- opt/scopedb/alembic/env.py
+++ opt/scopedb/alembic/env.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -6,34 +7,22 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# Load environment variables
-from scopeserver import settings
-from scopeserver import constants
-from scopedb import models
+from scopeserver.config import settings
+from scopeserver.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-# ---------------- added code here -------------------------#
-# this will overwrite the ini-file sqlalchemy.url path
-# with the path given in the config of the main code
-config.set_main_option("sqlalchemy.url", settings.get(key=constants.DATABASE_URL))
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
-
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+sys.path = [""] + sys.path
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+target_metadata = Base.metadata
 
 
 def run_migrations_offline():
