286ce6a7a414be2335ff9cb979db5dfbb945f736
dweemx <mdewaegeneer@gmail.com>
2020-10-22 19:06:24 +0200

    Abstract away lassoTab from ViewerSiderBar into LassoTab component

---
+++ src/components/LassoTool/LassoTab.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+
+import {
+    Tab,
+} from 'semantic-ui-react';
+
+import { LassoSelection } from "./model"
+import { LassoSelectionPane } from "./LassoSelectionPane"
+
+type LassoTabProps = {
+    selections: LassoSelection[]
+}
+
+export class LassoTab extends React.Component<LassoTabProps> {
+
+    constructor(props: LassoTabProps) {
+        super(props)
+    }
+
+    render() {
+        const { selections } = this.props;
+        if (selections.length == 0) {
+            return (
+                <Tab.Pane attached={false} style={{ textAlign: 'center' }}>
+                    <br />
+                    <br />
+                    No user&apos;s lasso selections
+                    <br />
+                    <br />
+                    <br />
+                </Tab.Pane>
+            );
+        }
+        
+        return selections.map((lassoSelection: LassoSelection, index: number) => {
+            return (
+                <LassoSelectionPane idx={index} {...lassoSelection}/>
+            )
+        });
+
+    }
+
+}
---
+++ src/components/LassoTool/model.ts
@@ -0,0 +1,7 @@
+export type LassoSelection = {
+    id: string;
+    points: any;
+    color: string;
+    selected: boolean;
+    clusterOverlaps?: any;
+}
