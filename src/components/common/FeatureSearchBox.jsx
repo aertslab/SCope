921c9f708c17daf7ae1cacceb7f1134b7301da3b
Kris Davie <kristofer.davie@kuleuven.vib.be>
2020-11-13 15:27:42 +0100

    Get loom metadata on feature update
    This causes the frontend to receive possible the latest annotations and metadata (such as votes or new annotations) when switching features. This means that when multiple people are working on the same file such as during a collaborative annotation session, all users see updates in real time without a reload required.

--- src/components/common/FeatureSearchBox.jsx
+++ src/components/common/FeatureSearchBox.jsx
@@ -69,12 +69,18 @@ export default class FeatureSearch extends React.Component {
 
     updateFeature(feature, featureType, featureDescription) {
         this.setState({ value: feature, selection: null });
-        BackendAPI.updateFeature(
-            this.props.field,
-            this.state.type,
-            feature,
-            featureType,
-            featureDescription
+        BackendAPI.queryLoomFiles(
+            BackendAPI.getUUID(),
+            () => {
+                BackendAPI.updateFeature(
+                    this.props.field,
+                    this.state.type,
+                    feature,
+                    featureType,
+                    featureDescription
+                );
+            },
+            BackendAPI.getActiveLoom()
         );
         ReactGA.event({
             category: 'action',
