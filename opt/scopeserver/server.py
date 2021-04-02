7e4106d1b53f7acd2bb446d723679317a053047d
dweemx <mdewaegeneer@gmail.com>
2021-04-02 11:45:05 +0200

    Comment orcid id related stuff. Will be used later

--- opt/scopeserver/server.py
+++ opt/scopeserver/server.py
@@ -1,6 +1,6 @@
 import logging
 
-from scopeserver.orcid import ORCID
+# from scopeserver.orcid import ORCID
 from scopeserver.dataserver.utils import data_file_handler as dfh
 from scopeserver.dataserver.utils import loom_file_handler as lfh
 
@@ -18,7 +18,7 @@ class SCopeServer:
         self.__data_handler.set_global_data()
         self.__dataset_handler = lfh.LoomFileHandler()
         self.__dataset_handler.set_global_data()
-        self.__orcid = ORCID(server=self)
+        # self.__orcid = ORCID(server=self)
 
     @property
     def config(self):
@@ -32,9 +32,9 @@ class SCopeServer:
     def dataset_handler(self):
         return self.__dataset_handler
 
-    @property
-    def orcid(self):
-        return self.__orcid
+    # @property
+    # def orcid(self):
+    #     return self.__orcid
 
     # TODO: Currently this is also defined in the legacy server! Out of sync issues => how best to deal with this ? Move instantation to new server ?
     def update_global_data(self) -> None:
