e744173612cb4bd90f9a8c1e9c60c614ac796ebd
dweemx <maxime.dewaegeneer@kuleuven.be>
2021-04-08 17:09:49 +0200

    Re-organize server according to https://github.com/aertslab/SCope/pull/418#pullrequestreview-628741667

--- opt/main.py
+++ opt/main.py
@@ -10,10 +10,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from scopeserver.config import from_file
 from scopeserver import message_of_the_day, LegacySCopeServer, SCopeServer
-from scopeapi import SCopeAPI
-from scopeapi.model import Error
-from scopeapi.model import GetUUID
-from scopeapi.model import GetRemainingUUIDTime, GetDatasets
+from scopeserver.api import SCopeAPI
+from scopeserver.api import scope_api_model
 
 scope_api = FastAPI()
 scope_api.add_middleware(
@@ -31,14 +29,14 @@ scope_server_legacy = LegacySCopeServer(config=CONFIG, new_server=scope_server)
 _scope_api = SCopeAPI(server=scope_server)
 
 
-@scope_api.get("/session/uuid/", response_model=GetUUID)
+@scope_api.get("/session/uuid/", response_model=scope_api_model.GetUUID)
 def get_uuid(ip: str):
     get_uuid_reply = _scope_api.get_uuid(ip=ip)
     """ A HTTP API endpoint to retrieve a new UUID. """
     return get_uuid_reply
 
 
-@scope_api.get("/session/{session_id}/info/", response_model=GetRemainingUUIDTime)
+@scope_api.get("/session/{session_id}/info/", response_model=scope_api_model.GetRemainingUUIDTime)
 def get_session_info(session_id, ip: str, mouse_events: int):
     get_remaining_uuid_time_reply = _scope_api.get_remaining_uuid_time(
         session_uuid=session_id, ip=ip, mouse_events=mouse_events
@@ -47,7 +45,7 @@ def get_session_info(session_id, ip: str, mouse_events: int):
     return get_remaining_uuid_time_reply
 
 
-@scope_api.get("/session/{session_id}/datasets/", response_model=GetDatasets)
+@scope_api.get("/session/{session_id}/datasets/", response_model=scope_api_model.GetDatasets)
 def get_datasets(session_id, dataset_file_name: str = None):
     get_datasets_reply = _scope_api.get_datasets(session_uuid=session_id, dataset_file_name=dataset_file_name)
     """ A HTTP API endpoint to retrieve a list of datasets. """
---
+++ opt/scopeserver/api/__init__.py
@@ -0,0 +1,2 @@
+from .v1 import SCopeAPI
+from .v1 import model as scope_api_model
