869e032e928f17e316ba49a028e2615ff0e971b8
James Collier <james.collier@vib.be>
2021-06-25 10:44:01 +0200

    Remove unused client deps

--- client/babel.config.js
+++ client/babel.config.js
@@ -5,11 +5,11 @@ module.exports = {
       {
         targets: {
           "esmodules": true
-        }
+        },
       }
     ],
     '@babel/preset-react'
   ],
   plugins: ['@babel/plugin-transform-runtime',
-           ["@babel/plugin-proposal-private-methods", { "loose": true }]]
+           ["@babel/plugin-proposal-private-methods", { "loose": false }]]
 };
