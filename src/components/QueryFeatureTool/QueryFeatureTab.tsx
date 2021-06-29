c67c02ff87c868408df2e865f6a4f633a623efb1
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2021-06-29 19:19:47 +0200

    Abstract away display of the metadata of the active feature into QueryFeatureMetadata component

--- src/components/QueryFeatureTool/QueryFeatureTab.tsx
+++ src/components/QueryFeatureTool/QueryFeatureTab.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { Grid, Tab } from 'semantic-ui-react';
-
-import GProfilerModal from '../GProfiler/GProfilerModal';
-import ClusterControls from '../ClusterControls';
-import { EmptyFeatureDisplayMessage } from '../QueryFeatureTool/EmptyFeatureDisplayMessage';
-import { MotifLogo } from '../MotifLogo';
-import CommunityAnnotationTable from '../CommunityAnnotationTable';
-import FeatureMarkerTable from '../FeatureMarkerTable';
-import LegendTable from '../LegendTable';
-import DownloadLoomButton from '../buttons/DownloadLoomButton';
+import { QueryFeatureMetadata } from '../QueryFeatureMetadata';
 
 type QueryFeatureTabProps = {
     history: any;
@@ -19,143 +11,28 @@ type QueryFeatureTabProps = {
     sessionIsRW: boolean;
 };
 
-export class QueryFeatureTab extends React.Component<QueryFeatureTabProps> {
-    constructor(props: QueryFeatureTabProps) {
-        super(props);
-    }
+export const QueryFeatureTab: React.FC<QueryFeatureTabProps> = (props) => {
+    const { activeFeature, activeFeatureIndex } = props;
 
-    showMotifLogo() {
-        const { activeFeature } = this.props;
-        return activeFeature.metadata?.motifName;
-    }
-
-    showClusterControls() {
-        const { activePage, activeFeature } = this.props;
-        return (
-            activeFeature.featureType.startsWith('Cluster') &&
-            activeFeature.feature !== 'All Clusters' &&
-            this.props.sessionIsRW &&
-            activePage === 'gene'
-        );
-    }
-
-    showCommunityAnnotationTable() {
-        const { activeFeature } = this.props;
-        return activeFeature.metadata?.cellTypeAnno;
-    }
-
-    showFeatureMarkerTable() {
-        const { activeFeature } = this.props;
-        return activeFeature.metadata?.genes;
-    }
-
-    showLegendTable() {
-        const { activeFeature } = this.props;
-        return (
-            this.props.activeLegend !== null &&
-            (activeFeature.featureType === 'annotation' ||
-                activeFeature.feature === 'All Clusters')
-        );
-    }
-
-    showDownloadLoomButton() {
-        const { activeFeature } = this.props;
-        return activeFeature.featureType.startsWith('Clustering');
-    }
-
-    showGProfilerModal() {
-        const { activeFeature } = this.props;
-        return activeFeature.featureType.startsWith('Clustering');
-    }
-
-    render() {
-        const { history, activePage, activeFeature, activeFeatureIndex } =
-            this.props;
-        let metadata = activeFeature?.feature ? (
-            ''
-        ) : (
-            <EmptyFeatureDisplayMessage featureIndex={activeFeatureIndex} />
-        );
-        if (activeFeature && activeFeature.metadata) {
-            const md = activeFeature.metadata;
-
-            metadata = (
+    return (
+        <Tab.Pane
+            attached={false}
+            key={activeFeatureIndex}
+            className={
+                'feature' + activeFeatureIndex + ' stretched marginBottom'
+            }
+            style={{ textAlign: 'center' }}>
+            <Grid>
                 <Grid.Row columns='1' centered className='viewerRow'>
-                    <Grid.Column stretched className='viewerCell'>
-                        {md.featureType}{' '}
-                        {activeFeature.featureType.startsWith('Clustering') &&
-                            `Group: ${md.clusteringGroup}`}{' '}
-                        {md.feature}
-                        <br />
-                        {this.showMotifLogo() && (
-                            <MotifLogo
-                                motifName={activeFeature.metadata.motifName}
-                            />
-                        )}
-                        {this.showClusterControls() && (
-                            <ClusterControls
-                                featureIndex={activeFeatureIndex}
-                                feature={activeFeature}
-                            />
-                        )}
-                        {this.showCommunityAnnotationTable() && (
-                            <CommunityAnnotationTable
-                                communityAnnotations={md.cellTypeAnno}
-                                activeFeature={activeFeature}
-                                sessionIsRW={this.props.sessionIsRW}
-                            />
-                        )}
-                        {this.showFeatureMarkerTable() && (
-                            <FeatureMarkerTable
-                                history={history}
-                                activePage={activePage}
-                                metadata={md}
-                                activeFeature={activeFeature}
-                                activeFeatureIndex={activeFeatureIndex}
-                            />
-                        )}
-                        {this.showLegendTable() && (
-                            <LegendTable
-                                activeLegend={this.props.activeLegend}
-                            />
-                        )}
-                        {this.showDownloadLoomButton() && (
-                            <DownloadLoomButton activeFeature={activeFeature} />
-                        )}
-                        {this.showGProfilerModal() && md.genes && (
-                            <GProfilerModal featureMetadata={md} />
-                        )}
-                        <br />
+                    <Grid.Column className='viewerCell'>
+                        {activeFeature ? activeFeature.featureType : ''}{' '}
+                        <b> {activeFeature ? activeFeature.feature : ''} </b>
                     </Grid.Column>
                 </Grid.Row>
-            );
-        }
-
-        return (
-            <Tab.Pane
-                attached={false}
-                key={activeFeatureIndex}
-                className={
-                    'feature' + activeFeatureIndex + ' stretched marginBottom'
-                }
-                style={{ textAlign: 'center' }}>
-                <Grid>
-                    <Grid.Row columns='1' centered className='viewerRow'>
-                        <Grid.Column className='viewerCell'>
-                            {activeFeature ? activeFeature.featureType : ''}{' '}
-                            <b>
-                                {' '}
-                                {activeFeature
-                                    ? activeFeature.feature
-                                    : ''}{' '}
-                            </b>
-                        </Grid.Column>
-                    </Grid.Row>
-                    {metadata}
-                </Grid>
-            </Tab.Pane>
-        );
-    }
-}
+                <QueryFeatureMetadata {...props} />
+            </Grid>
+        </Tab.Pane>
+    );
+};
 
 export default QueryFeatureTab;
