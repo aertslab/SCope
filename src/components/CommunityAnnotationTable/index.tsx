95088eb8616da1e02c56754c0d28dd3f743e1d33
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2021-06-29 22:49:07 +0200

    Fix bug declaration or statement expected.

--- src/components/CommunityAnnotationTable/index.tsx
+++ src/components/CommunityAnnotationTable/index.tsx
@@ -12,60 +12,58 @@ type CommunityAnnotationTableProps = {
     sessionIsRW: boolean;
 };
 
-const CommunityAnnotationTable: React.FC<CommunityAnnotationTableProps> = (props) => {
-
+const CommunityAnnotationTable: React.FC<CommunityAnnotationTableProps> = (
+    props
+) => {
     const header = () => {
         return 'Community Annotations';
-    }
+    };
 
-    const columns = (activeFeature)  => {
+    const columns = (activeFeature) => {
         return [
             asReactTableOBOColumn(),
             asReactTableCuratorColumn(),
             asReactTableVotesColumn(activeFeature),
         ];
-    }
+    };
 
-        const data = makeTableData(props.communityAnnotations);
+    const data = makeTableData(props.communityAnnotations);
 
-        if (props.communityAnnotations.length === 0) {
-            return (
-                <div
-                    style={{
-                        marginBottom: '5px',
-                        textAlign: 'center',
-                    }}>
-                    No annotations currently exist.{' '}
-                    {props.sessionIsRW
-                        ? 'Be the first to contribute!'
-                        : ''}
-                </div>
-            );
-        }
+    if (props.communityAnnotations.length === 0) {
         return (
             <div
                 style={{
-                    marginBottom: '15px',
+                    marginBottom: '5px',
                     textAlign: 'center',
                 }}>
-                <ReactTable
-                    data={data}
-                    columns={[
-                        {
-                            Header: header(),
-                            columns: columns(props.activeFeature),
-                        },
-                    ]}
-                    pageSizeOptions={[3]}
-                    defaultPageSize={3}
-                    // style={{
-                    // 	height: cellTypeAnnoTableHeight +"px" // This will force the table body to overflow and scroll, since there is not enough room
-                    // }}
-                    className='-striped -highlight'
-                />
+                No annotations currently exist.{' '}
+                {props.sessionIsRW ? 'Be the first to contribute!' : ''}
             </div>
         );
     }
-}
+    return (
+        <div
+            style={{
+                marginBottom: '15px',
+                textAlign: 'center',
+            }}>
+            <ReactTable
+                data={data}
+                columns={[
+                    {
+                        Header: header(),
+                        columns: columns(props.activeFeature),
+                    },
+                ]}
+                pageSizeOptions={[3]}
+                defaultPageSize={3}
+                // style={{
+                // 	height: cellTypeAnnoTableHeight +"px" // This will force the table body to overflow and scroll, since there is not enough room
+                // }}
+                className='-striped -highlight'
+            />
+        </div>
+    );
+};
 
 export default CommunityAnnotationTable;
