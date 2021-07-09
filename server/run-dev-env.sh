a1a655b9c59e36fef02ab009c05dfac67eb01537
James Collier <26444312+MaybeJustJames@users.noreply.github.com>
2021-07-09 17:15:37 +0200

    Set a default secret in the development environment

--- server/run-dev-env.sh
+++ server/run-dev-env.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "****************************************************************************************"
+echo "* WARNING: THIS SCRIPT SHOULD ONLY BE USED TO SET UP AND RUN A DEVELOPMENT ENVIRONMENT *"
+echo "*                      !! DO NOT USE THIS SCRIPT IN PRODUCTION !!                      *"
+echo "****************************************************************************************"
+
+export DATAHASHSECRET="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ab"
+
 poetry run dotenv run alembic upgrade head
 
 poetry run dotenv run hypercorn --log-level=debug main:scope_api --reload
