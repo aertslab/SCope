b60b8bb3111ae04601aa7a7c02736008b4468100
James Collier <james.collier@vib.be>
2021-07-26 22:04:41 +0200

    mark database functions as IO

--- server/scopeserver/api/deps.py
+++ server/scopeserver/api/deps.py
@@ -3,6 +3,8 @@
 from typing import Generator, Optional
 
 from fastapi import Cookie, Depends, HTTPException, status
+from returns.io import IOResult
+from returns.unsafe import unsafe_perform_io
 from sqlalchemy.orm import Session
 
 from scopeserver import crud, models, schemas
@@ -22,7 +24,7 @@ def get_current_user(database: Session = Depends(get_db), user_id: Optional[int]
     "Provide access to the User currently accessing the API."
     unknown_user = HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="Unknown user ID",
+        detail=f"Unknown user ID: {user_id}",
     )
 
     no_user_id = HTTPException(
@@ -32,9 +34,9 @@ def get_current_user(database: Session = Depends(get_db), user_id: Optional[int]
 
     if user_id is not None:
         user = crud.get_user(database, user=schemas.User(id=user_id))
-        if not user:
-            raise unknown_user
+        if isinstance(user, IOResult.success_type):
+            return unsafe_perform_io(user.unwrap())
 
-        return user
+        raise unknown_user
 
     raise no_user_id
