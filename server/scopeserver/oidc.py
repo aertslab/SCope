74d02bcc4e84bb59e191ecddf94a548d0ef81ef8
James Collier <james.collier@vib.be>
2021-08-06 16:02:08 +0200

    Make the HTTP client a dependency so that it can be mocked

--- server/scopeserver/oidc.py
+++ server/scopeserver/oidc.py
@@ -1,6 +1,6 @@
 " Abstractions over OpenID Connect APIs "
 
-from typing import Any, Dict, Optional, TypedDict
+from typing import Any, Dict, Optional
 from datetime import datetime, timedelta, timezone
 import base64
 from binascii import Error as Base64DecodeError
@@ -29,6 +29,50 @@ class TokenRequestBuilder:
         "scope": None,
         "redirect_uri": settings.AUTH_REDIRECT_URI,
     }
+
+    def with_clientid(self, clientid: str):
+        "Client id for requester (us)"
+        self._request.update({"client_id": clientid})
+        return self
+
+    def with_secret(self, secret: str):
+        "API provided secret"
+        self._request.update({"client_secret": secret})
+        return self
+
+    def with_grant_type(self, grant_type: str):
+        "Related to the type of flow used. Probably 'authorization_code'"
+        self._request.update({"grant_type": grant_type})
+        return self
+
+    def with_code(self, code: str):
+        "Code previously obtained from the authorisation endpoint"
+        self._request.update({"code": code})
+        return self
+
+    def with_state(self, state: str):
+        "State used by us to track anything we need between requests"
+        self._request.update({"state": state})
+        return self
+
+    def with_scope(self, scope: str):
+        "Scopes for the token"
+        self._request.update({"scope": scope})
+        return self
+
+    def with_redirect_uri(self, redirect_uri: str):
+        "The registered redirect URI after authentication"
+        self._request.update({"redirect_uri": redirect_uri})
+        return self
+
+    def build(self) -> Dict[str, Optional[str]]:
+        "Build the configuration after setting up"
+        return self._request.copy()
+
+
+async def configuration(client: httpx.AsyncClient, provider: str) -> ResponseType:
+    "Fetch a providers well-known openid configuration."
+    response = await client.get(f"{provider}/.well-known/openid-configuration")
 
     def with_clientid(self, clientid: str):
         "Client id for requester (us)"
